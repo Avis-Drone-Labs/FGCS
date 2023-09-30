0932866b52d488b42385eb3b204412af6440d27d
Kush Makkapati <kush.makkapati@icloud.com>
2023-09-30 18:59:51 +0100

    Add super simple GUI starter

--- gui/main.py
+++ gui/main.py
@@ -1,15 +1,70 @@
 import sys
-import PyQt6 as qt
-import PyQt6.QtWidgets as qtw
+
+from PyQt6.QtCore import Qt
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 800
+
+
+class PyCalcWindow(QMainWindow):
+    """The main window (GUI or view)."""
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Project Falcon GCS")
+        self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.generalLayout = QVBoxLayout()
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
+        self.setCentralWidget(centralWidget)
+        self._createLabels()
+
+    def _createLabels(self):
+        exampleTelemetry = [
+            ["Altitude", 9.2],
+            ["Airspeed", 12.5],
+            ["Groundspeed", 12.4],
+            ["Battery", "79%"],
+        ]
+        buttonsLayout = QGridLayout()
+
+        for idx, vals in enumerate(exampleTelemetry):
+            key = QLabel(f"{vals[0]}:")
+            font = key.font()
+            font.setPointSize(30)
+            key.setFont(font)
+
+            value = QLabel(str(vals[1]))
+            font = value.font()
+            font.setPointSize(30)
+            value.setFont(font)
+
+            buttonsLayout.addWidget(key, idx, 0)
+            buttonsLayout.addWidget(value, idx, 1)
+
+        buttonsLayout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.generalLayout.addLayout(buttonsLayout)
+
+
+def main():
+    """The main function."""
+    pycalcApp = QApplication([])
+    pycalcWindow = PyCalcWindow()
+    pycalcWindow.show()
+
+    sys.exit(pycalcApp.exec())
+
 
 if __name__ == "__main__":
-    app = qtw.QApplication([])
-
-    window = qtw.QWidget()
-    window.setWindowTitle("PyQt App")
-    window.setGeometry(100, 100, 280, 80)
-    helloMsg = qtw.QLabel("<h1>Hello, World!</h1>", parent=window)
-    helloMsg.move(60, 15)
-    window.show()
-
-    sys.exit(app.exec())
+    main()

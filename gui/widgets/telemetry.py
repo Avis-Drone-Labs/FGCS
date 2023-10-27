fb15849e4a16dac05d728f48425eb1dbd29cd36a
Julian <julianjones663@gmail.com>
2023-10-27 21:46:32 +0100

    added getters and setters for telemetry labels

--- gui/widgets/telemetry.py
+++ gui/widgets/telemetry.py
@@ -15,10 +15,10 @@ class TelemetryDataWidget(QWidget):
         # Create and display labels
         # TelemetryLabels will be in format {"labelName": [value, labelWidget]}
         self.telemetryLabels = {
-            "Altitude": [9.2, None],
-            "Airspeed": [12.5, None],
-            "Groundspeed": [12.4, None],
-            "Battery": ["79%", None],
+            "altitude": [9.2, None],
+            "airspeed": [12.5, None],
+            "groundspeed": [12.4, None],
+            "battery": ["79%", None],
         }
         self.labelFont = QFont("Ubuntu Mono", 30)
         self._createTelemetryLabels()
@@ -27,7 +27,7 @@ class TelemetryDataWidget(QWidget):
         """A function to create telemetry labels initially, to update use self._updateTElemetryLabels"""
         labelLayout: QGridLayout = QGridLayout()
         for index, (key, label) in enumerate(self.telemetryLabels.items()):
-            labelWidget = QLabel(f"{key}: {label[0]}")
+            labelWidget = QLabel(f"{key.capitalize()}: {label[0]}")
             labelWidget.setFont(self.labelFont)
             labelLayout.addWidget(labelWidget, index, 0)
             self.telemetryLabels[key][1] = labelWidget
@@ -35,3 +35,27 @@ class TelemetryDataWidget(QWidget):
         # Add labelLayout to generalLayout
         labelLayout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout.addLayout(labelLayout)
+
+    def updateTelemetryLabels(self, label_values: dict):
+        """
+        Update Telemetry Labels based from a dictionary, if the value in the dictionary
+        is not found then the value will be skipped (and ones not listed will not be
+        updated). For example: {"altitude": 12, "airspeed": 30}
+        """
+        for key, value in label_values.items():
+            if (
+                key in self.telemetryLabels.keys()
+                and self.telemetryLabels[key][1] is not None
+            ):
+                self.telemetryLabels[key][0] = value
+                self.telemetryLabels[key][1].setText(f"{key.capitalize()}: {value}")
+
+    def getTelemetryLabels(self):
+        """
+        Get all telemetry labels, very useful for updateTelemetryLabels
+        :return dict labels: A list of all label keys and current values
+        """
+        labels = {}
+        for key, value in self.telemetryLabels.items():
+            labels[key] = value[0]
+        return labels

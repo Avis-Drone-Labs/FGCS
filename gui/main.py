05961b5b156705527f7a433e9b03893010de19a4
Julian <julianjones663@gmail.com>
2023-10-27 21:46:32 +0100

    Add initial layout and business logic for GCS

--- gui/main.py
+++ gui/main.py
@@ -3,14 +3,17 @@ import sys
 from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
-    QVBoxLayout,
+    QHBoxLayout,
     QWidget,
 )
+from PyQt6.QtCore import QThreadPool
 
 from widgets.telemetry import TelemetryDataWidget
+from widgets.map import MapWidget
+from loops.telemetry_updater import TelemetryUpdaterLoop
 
-WINDOW_WIDTH = 1200
-WINDOW_HEIGHT = 700
+WINDOW_WIDTH = 1366
+WINDOW_HEIGHT = 768
 
 
 class MainWindow(QMainWindow):
@@ -23,17 +26,46 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(WINDOW_WIDTH, WINDOW_HEIGHT)
 
         # Layout Setup
-        self.generalLayout = QVBoxLayout()
+        self.general_layout = QHBoxLayout()
+
         centralWidget = QWidget(self)
-        centralWidget.setLayout(self.generalLayout)
+        centralWidget.setLayout(self.general_layout)
+        # centralWidget.setFixedWidth(700)
+
         self.setCentralWidget(centralWidget)
         self.telemetryWidget = TelemetryDataWidget()
-        self.generalLayout.addWidget(self.telemetryWidget)
+        self.general_layout.addWidget(self.telemetryWidget)
+
+        self.mapWidget = MapWidget()
+        self.general_layout.addWidget(self.mapWidget)
+
+        # Create threadpool
+        self.threadpool = QThreadPool()
+        self.activeThreads = []
+
+        # Add and run telemetry loop
+        self.telemetryUpdaterLoop = TelemetryUpdaterLoop(self.telemetryWidget)
+        self.activeThreads.append(self.telemetryUpdaterLoop)
+        self.threadpool.start(self.telemetryUpdaterLoop)
+
+    def closeEvent(self, _):
+        """Runs on GUI close, currently stopping all threads"""
+        self.threadpool.clear()
+        for thread in self.activeThreads:
+            try:
+                thread.stop()
+            except Exception as e:
+                print(f"Couldn't close thread: {e}")
 
 
 if __name__ == "__main__":
-    app = QApplication([])
+    app = QApplication(["test"])
     mainWindow = MainWindow()
     mainWindow.show()
+
+    # Set the stylesheet of the application
+    with open("gui/styles/styles.qss", "r") as f:
+        styles = f.read()
+        app.setStyleSheet(styles)
 
     sys.exit(app.exec())
--- gui/widgets/telemetry.py
+++ gui/widgets/telemetry.py
@@ -1,6 +1,5 @@
-from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QLabel
+from PyQt6.QtWidgets import QWidget, QGridLayout, QLabel
 
 
 class TelemetryDataWidget(QWidget):
@@ -9,29 +8,61 @@ class TelemetryDataWidget(QWidget):
     def __init__(self):
         # Widget Setup
         super().__init__(parent=None)
-        self.layout: QVBoxLayout = QVBoxLayout()
+        self.layout: QGridLayout = QGridLayout()
+        self.layout.setVerticalSpacing(0)
         self.setLayout(self.layout)
 
         # Create and display labels
         # TelemetryLabels will be in format {"labelName": [value, labelWidget]}
         self.telemetryLabels = {
-            "Altitude": [9.2, None],
-            "Airspeed": [12.5, None],
-            "Groundspeed": [12.4, None],
-            "Battery": ["79%", None],
+            "status": ["BOOT", None],
+            "altitude": [0, None],
+            "airspeed": [0, None],
+            "groundspeed": [0, None],
+            "battery_remaining": [0, None],
+            "battery_voltage": [0, None],
+            "battery_current": [0, None],
         }
-        self.labelFont = QFont("Ubuntu Mono", 30)
+        self.font = QFont("Ubuntu Mono", 30)
+
         self._createTelemetryLabels()
 
     def _createTelemetryLabels(self):
-        """A function to create telemetry labels initially, to update use self._updateTElemetryLabels"""
-        labelLayout: QGridLayout = QGridLayout()
+        """A function to create telemetry labels initially, to update use self._updateTelemetryLabels"""
         for index, (key, label) in enumerate(self.telemetryLabels.items()):
-            labelWidget = QLabel(f"{key}: {label[0]}")
-            labelWidget.setFont(self.labelFont)
-            labelLayout.addWidget(labelWidget, index, 0)
-            self.telemetryLabels[key][1] = labelWidget
+            keyWidget = QLabel(key)
+            keyWidget.setFont(self.font)
+            keyWidget.setObjectName("telemetry-key")
 
-        # Add labelLayout to generalLayout
-        labelLayout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.layout.addLayout(labelLayout)
+            valueWidget = QLabel(f"{label[0]}")
+            valueWidget.setFont(self.font)
+            valueWidget.setObjectName("telemetry-value")
+
+            self.layout.addWidget(keyWidget, index, 0)
+            self.layout.addWidget(valueWidget, index, 1)
+
+            self.telemetryLabels[key][1] = valueWidget
+
+    def updateTelemetryLabels(self, label_values: dict):
+        """
+        Update Telemetry Labels based from a dictionary, if the value in the dictionary
+        is not found then the value will be skipped (and ones not listed will not be
+        updated). For example: {"altitude": 12, "airspeed": 30}
+        """
+        for key, value in label_values.items():
+            if (
+                key in self.telemetryLabels.keys()
+                and self.telemetryLabels[key][1] is not None
+            ):
+                self.telemetryLabels[key][0] = value
+                self.telemetryLabels[key][1].setText(f"{value}")
+
+    def getTelemetryLabels(self):
+        """
+        Get all telemetry labels, very useful for updateTelemetryLabels
+        :return dict labels: A list of all label keys and current values
+        """
+        labels = {}
+        for key, value in self.telemetryLabels.items():
+            labels[key] = value[0]
+        return labels

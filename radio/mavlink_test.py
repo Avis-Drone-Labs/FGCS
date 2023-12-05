d2101c784c60a9321f6cb6108cb15b73b5840620
Kush Makkapati <kush.makkapati@icloud.com>
2023-12-05 09:16:03 +0000

    Add influxdb integration and upload mavlink data to influxdb

--- radio/mavlink_test.py
+++ radio/mavlink_test.py
@@ -4,7 +4,19 @@ import traceback
 from threading import Thread
 
 from pymavlink import mavutil
-from utils import secondsToMicroseconds
+from utils import secondsToMicroseconds, getComPort
+
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = "EKU_pTBZbvTIAF7mRPiNerKdS69vBXVY0zfXtWmvkdFLcD6DGGhel89J9IuzAjg9jljbuB06fQOJZIkI1rJ35g=="
+
+org = "Project Falcon"
+url = "http://localhost:8086"
+bucket = "telemetry"
+
+db_client = InfluxDBClient(url=url, token=token, org=org)
+write_api = db_client.write_api(write_options=SYNCHRONOUS)
 
 
 class Drone:
@@ -19,13 +31,13 @@ class Drone:
             f"Heartbeat received (system {self.target_system} component {self.target_component})"
         )
 
-        self.message_listeners = set()
+        self.message_listeners = {}
 
         self.is_active = True
 
         self.startThread()
 
-    def addMessageListener(self, message_id, interval=1):
+    def addMessageListener(self, message_id, func=None, interval=1):
         if message_id not in self.message_listeners:
             message = self.master.mav.command_long_encode(
                 self.target_system,  # Target system ID
@@ -49,7 +61,7 @@ class Drone:
             #     and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL
             #     and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED
             # ):
-            self.message_listeners.add(message_id)
+            self.message_listeners[message_id] = func
             return True
         return False
 
@@ -77,7 +89,7 @@ class Drone:
             #     and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL
             #     and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED
             # ):
-            self.message_listeners.remove(message_id)
+            del self.message_listeners[message_id]
             return True
         return False
 
@@ -98,7 +110,10 @@ class Drone:
                 print(traceback.format_exc())
                 msg = None
             if msg:
-                print(msg)
+                if self.message_listeners.get(msg.id):
+                    self.message_listeners[msg.id](msg)
+                # else:
+                # print(msg)
 
     def startThread(self):
         self.listener_thread = Thread(target=self.checkForMessages, daemon=True)
@@ -108,21 +123,89 @@ class Drone:
     def close(self):
         self.is_active = False
         for message_id in copy.deepcopy(self.message_listeners):
-            print(message_id)
             self.removeMessageListener(message_id)
         self.master.close()
 
         print("Closed connection to drone")
 
 
-port = "COM11"  # getComPort()
-drone = Drone(port)
+def sys_status_cb(msg):
+    print(msg.id)
+    point = (
+        Point("sys_status")
+        .field("voltage_battery", msg.voltage_battery / 1000)
+        .field("current_battery", msg.current_battery / 100)
+        .field("battery_remaining", msg.battery_remaining)
+    )
+    write_api.write(bucket=bucket, org="Project Falcon", record=point)
 
-drone.addMessageListener(mavutil.mavlink.MAVLINK_MSG_ID_ATTITUDE)
-print("Added message listener for attitude")
 
-try:
-    while True:
-        time.sleep(0.05)
-except KeyboardInterrupt:
-    drone.close()
+def attitude_cb(msg):
+    print(msg.id)
+    point = (
+        Point("attitude")
+        .field("pitch", msg.pitch)
+        .field("roll", msg.roll)
+        .field("yaw", msg.yaw)
+    )
+    write_api.write(bucket=bucket, org="Project Falcon", record=point)
+
+
+def gps_raw_int_cb(msg):
+    print(msg.id)
+    point = (
+        Point("gps_raw_int")
+        .field("lat", msg.lat)
+        .field("lon", msg.lon)
+        .field("satellites_visible", msg.satellites_visible)
+    )
+    write_api.write(bucket=bucket, org="Project Falcon", record=point)
+
+
+def vfr_hud_cb(msg):
+    print(msg.id)
+    point = (
+        Point("vfr_hud")
+        .field("airspeed", msg.airspeed)
+        .field("groundspeed", msg.groundspeed)
+        .field("heading", msg.heading)
+        .field("throttle", msg.throttle)
+        .field("altitude", msg.alt)
+    )
+    write_api.write(bucket=bucket, org="Project Falcon", record=point)
+
+
+def battery_status_cb(msg):
+    print(msg)
+
+
+def esc_status_cb(msg):
+    print(msg)
+
+
+if __name__ == "__main__":
+    # port = "COM11"
+    port = getComPort()
+    drone = Drone(port)
+
+    drone.addMessageListener(mavutil.mavlink.MAVLINK_MSG_ID_SYS_STATUS, sys_status_cb)
+    drone.addMessageListener(
+        mavutil.mavlink.MAVLINK_MSG_ID_ATTITUDE, attitude_cb, interval=0.25
+    )
+    drone.addMessageListener(mavutil.mavlink.MAVLINK_MSG_ID_GPS_RAW_INT, gps_raw_int_cb)
+    drone.addMessageListener(
+        mavutil.mavlink.MAVLINK_MSG_ID_VFR_HUD, vfr_hud_cb, interval=0.25
+    )
+    drone.addMessageListener(
+        mavutil.mavlink.MAVLINK_MSG_ID_BATTERY_STATUS, battery_status_cb
+    )
+    drone.addMessageListener(291, esc_status_cb)
+
+    try:
+        while True:
+            time.sleep(0.05)
+    except KeyboardInterrupt:
+        drone.close()
+
+    write_api.close()
+    db_client.close()

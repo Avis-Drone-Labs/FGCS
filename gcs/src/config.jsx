f5ca84d38b8097e11d045504a8679d8a00d52aa5
Kush Makkapati <kush.makkapati@icloud.com>
2024-02-27 12:25:51 +0000

    Add config page to frontend

--- gcs/src/config.jsx
+++ gcs/src/config.jsx
@@ -1,15 +1,15 @@
 import { useLocalStorage } from '@mantine/hooks'
 import Layout from './components/layout'
 
-export default function AllData() {
+export default function Config() {
   const [connected] = useLocalStorage({
     key: 'connectedToDrone',
     defaultValue: false,
   })
 
   return (
-    <Layout currentPage='all-data'>
-      <p>all data page</p>
+    <Layout currentPage='config'>
+      <p>Config page</p>
       <p>{connected}</p>
     </Layout>
   )

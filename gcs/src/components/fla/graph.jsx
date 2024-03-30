58366740713b3f334c4ae76c9a402a1fdd0eff70
Kush Makkapati <kush.makkapati@icloud.com>
2024-03-30 20:56:28 +0000

    94 fla graphs and streaming (#106)
    * plot setup added
    
    * Made styling and UX tweaks/improvements
    
    * changed some styling
    
    * created clear filter button
    
    * Add colour input element
    
    * Add functionality to download graph as image
    
    ---------
    
    Co-authored-by: Kwashie A <104215256+Kwash67@users.noreply.github.com>
    Co-authored-by: Julian <julianjones663@gmail.com>

--- gcs/src/components/fla/graph.jsx
+++ gcs/src/components/fla/graph.jsx
@@ -1,4 +1,10 @@
-import { Button } from '@mantine/core'
+import { ActionIcon, Tooltip as MantineTooltip } from '@mantine/core'
+import {
+  IconCapture,
+  IconZoomIn,
+  IconZoomOut,
+  IconZoomReset,
+} from '@tabler/icons-react'
 import {
   CategoryScale,
   Chart as ChartJS,
@@ -13,6 +19,22 @@ import {
 import zoomPlugin from 'chartjs-plugin-zoom'
 import { useRef } from 'react'
 import { Line } from 'react-chartjs-2'
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../../tailwind.config.js'
+
+// https://www.chartjs.org/docs/latest/configuration/canvas-background.html#color
+// Note: changes to the plugin code is not reflected to the chart, because the plugin is loaded at chart construction time and editor changes only trigger an chart.update().
+const customCanvasBackgroundColor = {
+  id: 'customCanvasBackgroundColor',
+  beforeDraw: (chart) => {
+    const { ctx } = chart
+    ctx.save()
+    ctx.globalCompositeOperation = 'destination-over'
+    ctx.fillStyle = '#242424'
+    ctx.fillRect(0, 0, chart.width, chart.height)
+    ctx.restore()
+  },
+}
 
 ChartJS.register(
   CategoryScale,
@@ -24,7 +46,10 @@ ChartJS.register(
   Legend,
   Colors,
   zoomPlugin,
+  customCanvasBackgroundColor,
 )
+
+const tailwindColors = resolveConfig(tailwindConfig).theme.colors
 
 function microsecondsToDisplayTime(microseconds, roundTo) {
   var seconds = microseconds / 1_000_000
@@ -75,13 +100,17 @@ const options = {
       },
       title: {
         display: true,
-        text: 'Time since boot (min:sec)',
+        text: 'Time since boot (mm:ss)',
       },
+      grid: { color: tailwindColors.gray[600] },
+    },
+    y: {
+      grid: { color: tailwindColors.gray[600] },
     },
   },
   elements: {
     point: {
-      radius: 0.75,
+      radius: 0,
     },
     line: {
       borderWidth: 1,
@@ -92,10 +121,79 @@ const options = {
 export default function Graph({ data }) {
   const chartRef = useRef(null)
 
+  function downloadUpscaledImage(originalDataURI, wantedWidth, wantedHeight) {
+    // https://stackoverflow.com/questions/20958078/resize-a-base-64-image-in-javascript-without-using-canvas
+    // We create an image to receive the Data URI
+    var img = document.createElement('img')
+
+    // When the event "onload" is triggered we can resize the image.
+    img.onload = function () {
+      // We create a canvas and get its context.
+      var canvas = document.createElement('canvas')
+      var ctx = canvas.getContext('2d')
+
+      // We set the dimensions at the wanted size.
+      canvas.width = wantedWidth
+      canvas.height = wantedHeight
+
+      // We resize the image with the canvas method drawImage();
+      ctx.drawImage(this, 0, 0, wantedWidth, wantedHeight)
+
+      var upscaledDataUri = canvas.toDataURL()
+
+      const link = document?.createElement('a')
+      link.download = 'graph.png'
+
+      link.href = upscaledDataUri
+
+      link.click()
+    }
+
+    // We put the Data URI in the image's src attribute
+    img.src = originalDataURI
+  }
+
+  function downloadGraphAsImage() {
+    const height = chartRef?.current?.height
+    const width = chartRef?.current?.width
+    downloadUpscaledImage(
+      chartRef?.current?.toBase64Image(),
+      width * 2,
+      height * 2,
+    )
+  }
+
   return (
     <div>
       <Line ref={chartRef} options={options} data={data} />
-      <Button onClick={chartRef?.current?.resetZoom}>Reset zoom</Button>
+      <div className='flex flex-row gap-2'>
+        <MantineTooltip label='Zoom in'>
+          <ActionIcon
+            variant='filled'
+            onClick={() => chartRef?.current?.zoom(1.5)}
+          >
+            <IconZoomIn size={18} />
+          </ActionIcon>
+        </MantineTooltip>
+        <MantineTooltip label='Zoom out'>
+          <ActionIcon
+            variant='filled'
+            onClick={() => chartRef?.current?.zoom(0)}
+          >
+            <IconZoomOut size={18} />
+          </ActionIcon>
+        </MantineTooltip>
+        <MantineTooltip label='Zoom reset'>
+          <ActionIcon variant='filled' onClick={chartRef?.current?.resetZoom}>
+            <IconZoomReset size={18} />
+          </ActionIcon>
+        </MantineTooltip>
+        <MantineTooltip label='Save graph as image'>
+          <ActionIcon variant='filled' onClick={downloadGraphAsImage}>
+            <IconCapture size={18} />
+          </ActionIcon>
+        </MantineTooltip>
+      </div>
     </div>
   )
 }
--- gcs/src/fla.jsx
+++ gcs/src/fla.jsx
@@ -1,16 +1,16 @@
 import {
   Accordion,
+  ActionIcon,
   Button,
   Checkbox,
-  FileInput,
-  Group,
-  Modal,
+  ColorInput,
+  FileButton,
   ScrollArea,
 } from '@mantine/core'
-import { useDisclosure } from '@mantine/hooks'
 import Layout from './components/layout'
 
-import { useEffect, useState } from 'react'
+import { IconPaint, IconTrash } from '@tabler/icons-react'
+import { Fragment, useEffect, useState } from 'react'
 import resolveConfig from 'tailwindcss/resolveConfig'
 import tailwindConfig from '../tailwind.config.js'
 import Graph from './components/fla/graph'
@@ -50,9 +50,7 @@ const presetCategories = [
 const ignoredKeys = ['TimeUS', 'function', 'source', 'result']
 
 export default function FLA() {
-  // States and disclosures used in react frontend
-  const [isModalOpen, { open: openModal, close: closeModal }] =
-    useDisclosure(false)
+  // States in react frontend
   const [file, setFile] = useState(null)
   const [loadingFile, setLoadingFile] = useState(false)
   const [logMessages, setLogMessages] = useState(null)
@@ -92,7 +90,6 @@ export default function FLA() {
 
         // Close modal and show success message
         showSuccessNotification(`${file.name} loaded successfully`)
-        closeModal()
       } else {
         // Error
         showErrorNotification(result.error)
@@ -112,6 +109,25 @@ export default function FLA() {
     })
     setMessageFilters(newFilters)
   }
+
+  // Turn off only one filter at a time
+  function removeDataset(label) {
+    let [categoryName, fieldName] = label.split('/')
+    let newFilters = { ...messageFilters }
+    if (
+      newFilters[categoryName] &&
+      newFilters[categoryName][fieldName] !== undefined
+    ) {
+      newFilters[categoryName][fieldName] = false
+    }
+    setMessageFilters(newFilters)
+  }
+
+  useEffect(() => {
+    if (file !== null) {
+      loadFile()
+    }
+  }, [file])
 
   useEffect(() => {
     if (!messageFilters) return
@@ -141,171 +157,192 @@ export default function FLA() {
     <Layout currentPage='fla'>
       {logMessages === null ? (
         // Open flight logs section
-        <>
-          <Modal
-            opened={isModalOpen}
-            onClose={closeModal}
-            title='Open Log File'
-            centered
-            overlayProps={{
-              backgroundOpacity: 0.55,
-              blur: 3,
-            }}
-            withCloseButton={false}
+        <div className='flex flex-col items-center justify-center h-full'>
+          <FileButton
+            variant='filled'
+            onChange={setFile}
+            accept='.log'
+            loading={loadingFile}
           >
-            <FileInput
-              variant='filled'
-              label='File Location'
-              description='Select a file to analyse'
-              placeholder='file.log'
-              onChange={setFile}
-              clearable
-              accept='.log'
-            />
-
-            <Group justify='space-between' className='pt-4'>
-              <Button
-                variant='filled'
-                color={tailwindColors.red[600]}
-                onClick={closeModal}
-              >
-                Close
-              </Button>
-              <Button
-                variant='filled'
-                color={tailwindColors.green[600]}
-                onClick={loadFile}
-                disabled={!file}
-                loading={loadingFile}
-              >
-                Analyse
-              </Button>
-            </Group>
-          </Modal>
-          <div className='flex flex-col w-max pl-10'>
-            No file loaded
-            <Button
-              variant='filled'
-              color={tailwindColors.green[600]}
-              onClick={openModal}
-              data-autofocus
-            >
-              Open File
-            </Button>
-          </div>
-        </>
+            {(props) => <Button {...props}>Analyse a log</Button>}
+          </FileButton>
+        </div>
       ) : (
         // Graphs section
-        <div className='flex gap-4 flex-cols h-3/4'>
-          {/* Message selection column */}
-          <div className='flex-none basis-1/4'>
-            <ScrollArea className='h-full max-h-max'>
-              <Accordion multiple={true}>
-                {/* Presets */}
-                <Accordion.Item key='presets' value='presets'>
-                  <Accordion.Control>Presets</Accordion.Control>
-                  <Accordion.Panel>
-                    <Accordion multiple={true}>
-                      {presetCategories.map((category) => {
-                        return (
-                          <Accordion.Item
-                            key={category.name}
-                            value={category.name}
-                          >
-                            <Accordion.Control>
-                              {category.name}
-                            </Accordion.Control>
-                            <Accordion.Panel>
-                              {category.filters.map((filter, idx) => {
-                                return (
-                                  <div className='pb-2'>
-                                    <Button
-                                      key={idx}
-                                      onClick={() => {
-                                        clearFilters()
-                                        let newFilters = { ...messageFilters }
-                                        Object.keys(filter.filters).map(
-                                          (categoryName) => {
-                                            if (
-                                              Object.keys(
-                                                messageFilters,
-                                              ).includes(categoryName)
-                                            ) {
-                                              filter.filters[categoryName].map(
-                                                (field) => {
+        <>
+          <div className='flex gap-4 flex-cols h-3/4'>
+            {/* Message selection column */}
+            <div className='w-1/4 pb-6'>
+              <ScrollArea className='h-full max-h-max'>
+                <Accordion multiple={true}>
+                  {/* Presets */}
+                  <Accordion.Item key='presets' value='presets'>
+                    <Accordion.Control>Presets</Accordion.Control>
+                    <Accordion.Panel>
+                      <Accordion multiple={true}>
+                        {presetCategories.map((category) => {
+                          return (
+                            <Accordion.Item
+                              key={category.name}
+                              value={category.name}
+                            >
+                              <Accordion.Control>
+                                {category.name}
+                              </Accordion.Control>
+                              <Accordion.Panel>
+                                <div className='flex flex-col gap-2'>
+                                  {category.filters.map((filter, idx) => {
+                                    return (
+                                      <Button
+                                        key={idx}
+                                        onClick={() => {
+                                          clearFilters()
+                                          let newFilters = { ...messageFilters }
+                                          Object.keys(filter.filters).map(
+                                            (categoryName) => {
+                                              if (
+                                                Object.keys(
+                                                  messageFilters,
+                                                ).includes(categoryName)
+                                              ) {
+                                                filter.filters[
+                                                  categoryName
+                                                ].map((field) => {
                                                   newFilters[categoryName][
                                                     field
                                                   ] = true
-                                                },
-                                              )
-                                            } else {
-                                              showErrorNotification(
-                                                `Your log file does not include ${categoryName}`,
-                                              )
-                                            }
-                                          },
-                                        )
-                                        setMessageFilters(newFilters)
-                                      }}
-                                    >
-                                      {filter.name}
-                                    </Button>
-                                  </div>
-                                )
-                              })}
-                            </Accordion.Panel>
-                          </Accordion.Item>
-                        )
-                      })}
-                    </Accordion>
-                  </Accordion.Panel>
-                </Accordion.Item>
+                                                })
+                                              } else {
+                                                showErrorNotification(
+                                                  `Your log file does not include ${categoryName}`,
+                                                )
+                                              }
+                                            },
+                                          )
+                                          setMessageFilters(newFilters)
+                                        }}
+                                      >
+                                        {filter.name}
+                                      </Button>
+                                    )
+                                  })}
+                                </div>
+                              </Accordion.Panel>
+                            </Accordion.Item>
+                          )
+                        })}
+                      </Accordion>
+                    </Accordion.Panel>
+                  </Accordion.Item>
 
-                {/* All messages */}
-                <Accordion.Item key='messages' value='messages'>
-                  <Accordion.Control>Messages</Accordion.Control>
-                  <Accordion.Panel>
-                    <Accordion multiple={false}>
-                      {Object.keys(messageFilters).map((messageName, idx) => {
-                        return (
-                          <Accordion.Item key={idx} value={messageName}>
-                            <Accordion.Control>{messageName}</Accordion.Control>
-                            <Accordion.Panel>
-                              {Object.keys(messageFilters[messageName]).map(
-                                (fieldName, idx) => {
-                                  return (
-                                    <Checkbox
-                                      key={idx}
-                                      label={fieldName}
-                                      checked={
-                                        messageFilters[messageName][fieldName]
-                                      }
-                                      onChange={(event) => {
-                                        let newFilters = { ...messageFilters }
-                                        newFilters[messageName][fieldName] =
-                                          event.currentTarget.checked
-                                        setMessageFilters(newFilters)
-                                      }}
-                                    />
-                                  )
-                                },
-                              )}
-                            </Accordion.Panel>
-                          </Accordion.Item>
-                        )
-                      })}
-                    </Accordion>
-                  </Accordion.Panel>
-                </Accordion.Item>
-              </Accordion>
-            </ScrollArea>
+                  {/* All messages */}
+                  <Accordion.Item key='messages' value='messages'>
+                    <Accordion.Control>Messages</Accordion.Control>
+                    <Accordion.Panel>
+                      <Accordion multiple={false}>
+                        {Object.keys(messageFilters).map((messageName, idx) => {
+                          return (
+                            <Accordion.Item key={idx} value={messageName}>
+                              <Accordion.Control>
+                                {messageName}
+                              </Accordion.Control>
+                              <Accordion.Panel>
+                                {Object.keys(messageFilters[messageName]).map(
+                                  (fieldName, idx) => {
+                                    return (
+                                      <Checkbox
+                                        key={idx}
+                                        label={fieldName}
+                                        checked={
+                                          messageFilters[messageName][fieldName]
+                                        }
+                                        onChange={(event) => {
+                                          let newFilters = { ...messageFilters }
+                                          newFilters[messageName][fieldName] =
+                                            event.currentTarget.checked
+                                          setMessageFilters(newFilters)
+                                        }}
+                                      />
+                                    )
+                                  },
+                                )}
+                              </Accordion.Panel>
+                            </Accordion.Item>
+                          )
+                        })}
+                      </Accordion>
+                    </Accordion.Panel>
+                  </Accordion.Item>
+                </Accordion>
+              </ScrollArea>
+            </div>
+
+            {/* Graph column */}
+            <div className='w-full h-full pr-4'>
+              <Graph data={chartData} />
+            </div>
           </div>
 
-          {/* Graph column */}
-          <div className='basis-3/4 pr-4'>
-            <Graph data={chartData} />
+          {/* Plots Setup */}
+          <div className='flex gap-4 pt-6 flex-cols h-1/4'>
+            <div className='ml-4'>
+              <div className='flex flex-row items-center'>
+                <h3 className='mt-2 mb-2 text-xl'>Graph setup</h3>
+                {/* Clear Filters */}
+                <Button
+                  className='ml-6'
+                  size='xs'
+                  color={tailwindColors.red[500]}
+                  onClick={clearFilters}
+                >
+                  Clear graph
+                </Button>
+              </div>
+              {chartData.datasets.map((item, index) => (
+                <Fragment key={index}>
+                  <div className='inline-flex items-center px-2 py-2 mr-3 text-xs font-bold text-white border border-gray-700 rounded-lg bg-grey-200 gap-2'>
+                    {/* Name */}
+                    <span>{item.label}</span>
+
+                    {/* Color Selector */}
+                    <ColorInput
+                      className='w-32'
+                      format='hex'
+                      swatches={[
+                        '#f5f5f5',
+                        '#868e96',
+                        '#fa5252',
+                        '#e64980',
+                        '#be4bdb',
+                        '#7950f2',
+                        '#4c6ef5',
+                        '#228be6',
+                        '#15aabf',
+                        '#12b886',
+                        '#40c057',
+                        '#82c91e',
+                        '#fab005',
+                        '#fd7e14',
+                      ]}
+                      closeOnColorSwatchClick
+                      withEyeDropper={false}
+                      rightSection={<IconPaint size={18} />}
+                      onChangeEnd={(color) => console.log(color)} // TODO: Add updating colors
+                    />
+                    {/* Delete button */}
+                    <ActionIcon
+                      variant='subtle'
+                      color={tailwindColors.red[500]}
+                      onClick={() => removeDataset(item.label)}
+                    >
+                      <IconTrash size={18} />
+                    </ActionIcon>
+                  </div>
+                </Fragment>
+              ))}
+            </div>
           </div>
-        </div>
+        </>
       )}
     </Layout>
   )

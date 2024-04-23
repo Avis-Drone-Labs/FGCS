dfb506cffc8119f1b173dfe3a9b0ae1c0d06faf9
Joe <joantpat@gmail.com>
2024-04-23 15:36:54 +0100

    Moved rowItem to components

---
+++ gcs/src/components/params/rowItem.jsx
@@ -0,0 +1,33 @@
+/*
+  File description
+*/
+
+import { memo } from "react"
+import ValueInput from "./valueInput"
+import { ScrollArea, Tooltip } from "@mantine/core"
+import apmParamDefs from '../data/gen_apm_params_def.json'
+
+
+const RowItem = memo(({ param, style, onChange }) => {
+  const paramDef = apmParamDefs[param.param_id]
+  return (
+    <div style={style} className='flex flex-row items-center space-x-4'>
+      <Tooltip label={paramDef?.DisplayName}>
+        <p className='w-56'>{param.param_id}</p>
+      </Tooltip>
+      <ValueInput
+        param={param}
+        paramDef={paramDef}
+        onChange={onChange}
+        className='w-3/12'
+      />
+      <div className='w-1/2'>
+        <ScrollArea.Autosize className='max-h-24'>
+          {paramDef?.Description}
+        </ScrollArea.Autosize>
+      </div>
+    </div>
+  )
+})
+
+export default RowItem

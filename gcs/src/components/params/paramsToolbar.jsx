45a9c28d2c88b03d3c391f555ec873a8cf3d8027
Joe Paton <joantpat@gmail.com>
2024-04-25 17:27:46 +0100

    Fixed indent

--- gcs/src/components/params/paramsToolbar.jsx
+++ gcs/src/components/params/paramsToolbar.jsx
@@ -1,8 +1,8 @@
 /*
-    Component containing the toolbar for the parameters page
+Component containing the toolbar for the parameters page
 
-    Contains the search bar, as well as the buttons for toggling shown parameters, saving and refreshing parameters, and 
-    rebooting the autopilot
+Contains the search bar, as well as the buttons for toggling shown parameters, saving and refreshing parameters, and 
+rebooting the autopilot
 */
 
 // 3rd party imports
@@ -18,14 +18,15 @@ import { socket } from '../../helpers/socket.js'
 
 const tailwindColors = resolveConfig(tailwindConfig).theme.colors
 
-/**
- * Sets all the modified parameters to their new values on the drone
- */
-function saveModifiedParams() {
-  socket.emit('set_multiple_params', modifiedParams)
-}
-
 export default function ParamsToolbar({searchValue, modifiedParams, showModifiedParams, refreshCallback, rebootCallback, modifiedCallback, searchCallback}){
+  
+  /**
+  * Sets all the modified parameters to their new values on the drone
+  */
+  function saveModifiedParams() {
+    socket.emit('set_multiple_params', modifiedParams)
+  }
+  
   return (
     <div className='flex space-x-4 justify-center'>
       <Tooltip
@@ -38,14 +39,14 @@ export default function ParamsToolbar({searchValue, modifiedParams, showModified
           color={tailwindColors.orange[600]}
         > {showModifiedParams ? <IconEye size={14} /> : <IconTool size={14} />} </Button>
       </Tooltip>
-
+      
       <TextInput
         className='w-1/3'
         placeholder='Search by parameter name'
         value={searchValue}
         onChange={(event) => searchCallback(event.currentTarget.value)}
       />
-
+      
       <Button
         size='sm'
         rightSection={<IconPencil size={14} />}
@@ -53,14 +54,14 @@ export default function ParamsToolbar({searchValue, modifiedParams, showModified
         onClick={saveModifiedParams}
         color={tailwindColors.green[600]}
       > Save params </Button>
-
+      
       <Button
         size='sm'
         rightSection={<IconRefresh size={14} />}
         onClick={refreshCallback}
         color={tailwindColors.blue[600]}
       > Refresh params </Button>
-
+      
       <Button
         size='sm'
         rightSection={<IconPower size={14} />}

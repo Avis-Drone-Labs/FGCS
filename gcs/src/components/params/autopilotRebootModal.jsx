c853452a71b1fac6db16c035899c63c85853c726
Joe <joantpat@gmail.com>
2024-04-25 19:45:58 +0100

    152 params refactor (#159)
    * Separated file into modular components
    
    * Moved bitmaskselect to components
    
    * Moved valueInput to components
    
    * Moved rowItem to components
    
    * Moved row to component
    
    * Cleaned up imports
    
    * Moved autopilot reboot modal to new file
    
    * Moved autopilot reboot modal to new file
    
    * Added return statement
    
    * Update params header
    
    * Fixed imports
    
    * Added file description
    
    * Added file description
    
    * Renamed paramsSearchbar to be more descriptive
    
    * Added comments
    
    * Updated comments
    
    * Fixed issue with fixedsizelist causing crash
    
    * Fixed indent
    
    * Added spacing
    
    * Update function comments
    
    * made styling more consistent by adding/removing emtpy lines and grouping tailwind colours together
    
    ---------
    
    Co-authored-by: Julian <julianjones663@gmail.com>

---
+++ gcs/src/components/params/autopilotRebootModal.jsx
@@ -0,0 +1,52 @@
+/*
+    Modal that pops up when rebooting the autopilot using the "Reboot FC" button
+*/
+
+// 3rd party imports
+import { Button, Loader, Modal } from "@mantine/core";
+
+// Styling imports
+import tailwindConfig from '../../../tailwind.config.js'
+import resolveConfig from 'tailwindcss/resolveConfig'
+const tailwindColors = resolveConfig(tailwindConfig).theme.colors
+
+export default function AutopilotRebootModal({rebootData, opened, onClose}){
+  return (
+    <Modal
+      opened={opened}
+      onClose={onClose}
+      title='Rebooting autopilot'
+      closeOnClickOutside={false}
+      closeOnEscape={false}
+      withCloseButton={false}
+      centered
+      overlayProps={{
+        backgroundOpacity: 0.55,
+        blur: 3,
+      }}
+    >
+      <div className='flex flex-col items-center justify-center'>
+        {rebootData.message === undefined ? (
+          <Loader />
+        ) : (
+          <>
+          {!rebootData.success && (
+            <>
+              <p className='my-2'>
+                {rebootData.message} You will need to reconnect.
+              </p>
+              <Button
+                onClick={close}
+                color={tailwindColors.red[600]}
+                className='mt-4'
+              >
+                Close
+              </Button>
+            </>
+          )}
+          </>
+        )}
+      </div>
+    </Modal>
+  )
+}

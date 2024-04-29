4c6fec17c4d225b128c27a0f90d5aff7a8475b6c
jedsmith2004 <90654430+jedsmith2004@users.noreply.github.com>
2024-04-29 16:54:42 +0100

    WIP changing height to position on map

--- gcs/src/components/fla/droneFlightPath.jsx
+++ gcs/src/components/fla/droneFlightPath.jsx
@@ -1,11 +1,11 @@
 // DroneFlightPath.jsx
 import { useState, useEffect } from 'react';
-import { 
-  Ion, 
-  Cartesian3, 
-  Color, 
-  IonResource, 
-  createWorldTerrainAsync, 
+import {
+  Ion,
+  Cartesian3,
+  Color,
+  IonResource,
+  createWorldTerrainAsync,
   createOsmBuildingsAsync,
   JulianDate,
   SampledPositionProperty,
@@ -13,17 +13,19 @@ import {
   TimeIntervalCollection,
   PathGraphics,
   VelocityOrientationProperty,
-  HeightReference
+  HeightReference,
+  Cartographic,
+  sampleTerrain
 } from "cesium";
-import { 
+import {
   Viewer,
-  Entity, 
-  Clock 
+  Entity,
+  Clock
 } from "resium";
 import data from './flightdata.json'
 
-Ion.defaultAccessToken='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkODdjNjMyYy00OGQ4LTQzNmUtYTQ1MC0xYmVmN2VmYWY2YzkiLCJpZCI6MjA1ODQ4LCJpYXQiOjE3MTE5ODMzMDR9.QumArMVhHylPYjVV1hqI38aMPG1vVd6RXBsX4wNDzKg'
-  
+Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkODdjNjMyYy00OGQ4LTQzNmUtYTQ1MC0xYmVmN2VmYWY2YzkiLCJpZCI6MjA1ODQ4LCJpYXQiOjE3MTE5ODMzMDR9.QumArMVhHylPYjVV1hqI38aMPG1vVd6RXBsX4wNDzKg'
+
 /* Initialize the viewer clock:
   Assume the radar samples are 30 seconds apart, and calculate the entire flight duration based on that assumption.
   Get the start and stop date times of the flight, where the start is the known flight departure time (converted from PST 
@@ -55,7 +57,7 @@ Ion.defaultAccessToken='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkODdjNjM
 //   );
 // });
 
-const DroneFlightPath = ({gpsData}) => {
+const DroneFlightPath = async ({ gpsData }) => {
   const [terrainProvider, setTerrainProvider] = useState(null);
   const [osmBuildings, setOsmBuildings] = useState(null); // idk how to use this yet
 
@@ -66,30 +68,12 @@ const DroneFlightPath = ({gpsData}) => {
 
   const positionProperty = new SampledPositionProperty();
   //console.log(gpsData);
-  const altOffset = gpsData[0].height;
 
-  const flightData = gpsData.map((item, index)=> {
-    // Declare the time for this individual sample and store it in a new JulianDate instance.
-    const time = JulianDate.addSeconds(start, index * timeStepInSeconds, new JulianDate());
-    const position = Cartesian3.fromDegrees(item.longitude, item.latitude, item.height);
-    //console.log(position.toString())
-    // Store the position along with its timestamp.
-    // Here we add the positions all upfront, but these can be added at run-time as samples are received from a server.
-    positionProperty.addSample(time, position);
-    return(
-      <Entity
-      key={index}
-      position = {position}
-      heightReference= {HeightReference.RELATIVE_TO_GROUND}
-      point={{pixelSize:'4', color:Color.RED}}
-    />
-    );
-  });
   useEffect(() => {
     createWorldTerrainAsync({
       url: IonResource.fromAssetId(1),
-      requestWaterMask : true, // required for water effects
-      requestVertexNormals : true // required for terrain lighting
+      requestWaterMask: true, // required for water effects
+      requestVertexNormals: true // required for terrain lighting
     }).then(tp => setTerrainProvider(tp));
 
     createOsmBuildingsAsync().then(buildings => setOsmBuildings(buildings));
@@ -99,29 +83,72 @@ const DroneFlightPath = ({gpsData}) => {
     return null; // or a loading spinner
   }
 
-  const droneLine = <Entity 
-    availability={new TimeIntervalCollection([new TimeInterval({start:start,stop:stop})])}
+  async function getHeight() {
+    try {
+      var samples = await sampleTerrain(terrainProvider, 11, [Cartographic.fromDegrees(parseFloat(gpsData[0].longitude), parseFloat(gpsData[0].latitude))]);
+      return samples[0].height;
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // getHeight().then(function(height) {
+  //   if (height == null) {
+  //     height = 0;
+  //     console.error("Failed to retrieve height.");
+  //   }
+  //   else {
+  //     console.log("HI IT WORKS")
+  //   }
+
+  // });
+
+  const flightData = gpsData.map(async (item, index) => {
+    // Declare the time for this individual sample and store it in a new JulianDate instance.
+    const time = JulianDate.addSeconds(start, index * timeStepInSeconds, new JulianDate());
+
+    const position = Cartesian3.fromDegrees(item.longitude, item.latitude, item.height + await getHeight());
+    console.log(position)
+    //console.log(position.toString())
+    // Store the position along with its timestamp.
+    // Here we add the positions all upfront, but these can be added at run-time as samples are received from a server.
+    positionProperty.addSample(time, position);
+    return (
+      <Entity
+        key={index}
+        position={position}
+        heightReference={HeightReference.RELATIVE_TO_GROUND}
+        point={{ pixelSize: '4', color: Color.RED }}
+      />
+    );
+  });
+
+  console.log(await flightData)
+
+
+  const droneLine = <Entity
+    availability={new TimeIntervalCollection([new TimeInterval({ start: start, stop: stop })])}
     position={positionProperty}
     // model={{
     //   uri:'./droneModel/drone.gltf',
     //   minimumPixelSize:28, // adjust drone size here
     //   maximumScale:50
     // }}
-    point= { {pixelSize: 25, color: Color.GREEN} } // for testing
+    point={{ pixelSize: 25, color: Color.GREEN }} // for testing
     orientation={new VelocityOrientationProperty(positionProperty)}
-    path={new PathGraphics({width:3})}
+    path={new PathGraphics({ width: 3 })}
     tracked
     selected
   />
 
   return (
     <Viewer terrainProvider={terrainProvider}>
-      <Clock 
-      startTime={start.clone()}
-      stopTime={stop.clone()}
-      currentTime={start.clone()}
-      multiplier={50}
-      shouldAnimate={true}>
+      <Clock
+        startTime={start.clone()}
+        stopTime={stop.clone()}
+        currentTime={start.clone()}
+        multiplier={50}
+        shouldAnimate={true}>
         {flightData}
         {droneLine}
       </Clock>

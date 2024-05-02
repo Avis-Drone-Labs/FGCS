8f656e659781d38d9577a5839baa80d21f5d55e9
Julian Jones <julianjones663@gmail.com>
2024-05-02 20:23:47 +0100

    added refresh parms

--- radio/app/endpoints/params.py
+++ radio/app/endpoints/params.py
@@ -1,3 +1,4 @@
+import time
 from .. import socketio
 import app.drone as droneStatus
 
@@ -25,3 +26,48 @@ def set_multiple_params(params_list):
         )
     else:
         socketio.emit("params_error", {"message": "Failed to save parameters."})
+
+
+@socketio.on("refresh_params")
+def refresh_params():
+    """
+    Refresh all parameters
+    """
+    if droneStatus.state != "params":
+        socketio.emit(
+            "params_error",
+            {"message": "You must be on the params screen to refresh the parameters."},
+        )
+        print(f"Current state: {droneStatus.state}")
+        return
+
+    droneStatus.drone.getAllParams()
+
+    timeout = time.time() + 60 * 3  # 3 minutes from now
+    last_index_sent = -1
+
+    while droneStatus.drone and droneStatus.drone.is_requesting_params:
+        if time.time() > timeout:
+            socketio.emit(
+                "params_error",
+                {"message": "Parameter request timed out after 3 minutes."},
+            )
+            return
+
+        if (
+            last_index_sent != droneStatus.current_param_index
+            and droneStatus.drone.current_param_index > last_index_sent
+        ):
+            socketio.emit(
+                "param_request_update",
+                {
+                    "current_param_index": droneStatus.drone.current_param_index,
+                    "total_number_of_params": droneStatus.drone.total_number_of_params,
+                },
+            )
+            last_index_sent = droneStatus.drone.current_param_index
+
+        time.sleep(0.2)
+
+    if droneStatus.drone:
+        socketio.emit("params", droneStatus.drone.params)

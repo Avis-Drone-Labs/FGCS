3f8c183671ef792800014260f6d1a9cdd400e454
Julian Jones <julianjones663@gmail.com>
2024-05-04 20:53:13 +0100

    removed influx DB

--- radio/mavlink_test.py
+++ radio/mavlink_test.py
@@ -4,19 +4,6 @@ from pprint import pprint
 from app.drone import Drone
 from app.utils import getComPort
 
-# from influxdb_client import InfluxDBClient, Point
-# from influxdb_client.client.write_api import SYNCHRONOUS
-
-# token = "EKU_pTBZbvTIAF7mRPiNerKdS69vBXVY0zfXtWmvkdFLcD6DGGhel89J9IuzAjg9jljbuB06fQOJZIkI1rJ35g=="
-
-# org = "Project Falcon"
-# url = "http://localhost:8086"
-# bucket = "telemetry"
-
-# db_client = InfluxDBClient(url=url, token=token, org=org)
-# write_api = db_client.write_api(write_options=SYNCHRONOUS)
-
-
 def test_cb(msg=None):
     print(msg)
     # print(datetime.datetime.now().time(), datetime.datetime.fromtimestamp(msg._timestamp).time(), msg.rpm)

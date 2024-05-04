f0d0b41181fa3f0e4f07490539f67968955e67c8
Julian Jones <julianjones663@gmail.com>
2024-05-04 21:00:39 +0100

    ignored imports for ruff

--- radio/app/endpoints/__init__.py
+++ radio/app/endpoints/__init__.py
@@ -3,14 +3,13 @@ from flask import Blueprint
 endpoints = Blueprint("endpoints", __name__)
 
 # Ignore warnings from VSCode, these are used in /app/__init__.py for registering the blueprint
-# Ignore F401.
-from . import arm
-from . import autopilot
-from . import comPorts
-from . import connections
-from . import flightMode
-from . import gripper
-from . import mission
-from . import motors
-from . import params
-from . import states
+from . import arm # noqa: F401, E402
+from . import autopilot # noqa: F401, E402
+from . import comPorts # noqa: F401, E402
+from . import connections # noqa: F401, E402
+from . import flightMode # noqa: F401, E402
+from . import gripper # noqa: F401, E402
+from . import mission # noqa: F401, E402
+from . import motors # noqa: F401, E402
+from . import params # noqa: F401, E402
+from . import states # noqa: F401, E402

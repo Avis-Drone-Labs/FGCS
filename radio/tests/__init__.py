72e2cfc29716f67f2e0ff539556c039c951b4b4d
Julian Jones <julianjones663@gmail.com>
2024-05-12 16:00:49 +0100

    wrote a better wrapper

--- radio/tests/__init__.py
+++ radio/tests/__init__.py
@@ -6,27 +6,44 @@ from typing import Callable
 from app import create_app, socketio
 from flask_socketio.test_client import SocketIOTestClient
 from flask.testing import FlaskClient
+import app.droneStatus as droneStatus
 
 app = create_app(debug=True)
 socketio = socketio
 
 
-def falcon_test(test_func: Callable):
+def falcon_test(pass_drone: bool = False, pass_flask: bool = False):
     """
-    Useful wrapper for a test function on project falcon
+    A wrapper to connect to backend and pass necessary details to test function
 
     Args:
-        test_func (callable): The function you want to test
+        pass_drone (bool): True if you want to accept the droneStatus file to the test func
+        pass_flask (bool): True if you want to accept the flask_test_client file to the test func
     """
 
-    def inner(*args, **kwargs):
-        # Create flask/socketio client to be used in each test
-        flask_client: FlaskClient = app.test_client()
-        socketio_client: SocketIOTestClient = socketio.test_client(app, flask_test_client=flask_client)
+    def run_test(test_func: Callable = None):
+        """Inner wrapper to run test"""
 
-        # Make sure the server did not rejected the connection
-        assert socketio_client.is_connected()
+        if test_func is None:
+            raise ValueError("You forgot to write parenthesis around falcon_test() so test_func was set to None")
 
-        test_func(flask_client, socketio_client, *args, **kwargs)
+        def inner(*args, **kwargs):
+            # Create flask/socketio client to be used in each test
+            flask_client: FlaskClient = app.test_client()
+            socketio_client: SocketIOTestClient = socketio.test_client(app, flask_test_client=flask_client)
 
-    return inner
+            # Make sure the server did not rejected the connection
+            assert socketio_client.is_connected()
+
+            # Get variables to pass into test_func
+            passing_variables = {}
+            if pass_drone:
+                passing_variables["droneStatus"] = droneStatus
+            if pass_flask:
+                passing_variables["flask_client"] = flask_client
+
+            # Run test
+            test_func(socketio_client, *args, **passing_variables, **kwargs)
+
+        return inner
+    return run_test
--- radio/tests/test_connections.py
+++ radio/tests/test_connections.py
@@ -1,17 +1,38 @@
 from . import falcon_test
 from flask_socketio.test_client import SocketIOTestClient
-from flask.testing import FlaskClient
 
 
-@falcon_test
-def test_connection(flask_client: FlaskClient, socketio_client: SocketIOTestClient):
-    """
-    Test arming function
-    """
-    flask_result = flask_client.post("/connect")
-    assert flask_result.status_code == 404  # No result sent back so error 404
-
+@falcon_test()
+def test_connection(socketio_client: SocketIOTestClient):
+    """Test connecting to socket"""
+    socketio_client.emit("/connect")
     socketio_result = socketio_client.get_received()
     assert len(socketio_result) == 0  # No message sent back
 
     
+@falcon_test(pass_drone=True)
+def test_disconnect(socketio_client: SocketIOTestClient, droneStatus):
+    """Test disconnecting from socket"""
+    socketio_client.emit("/disconnect")
+    socketio_result = socketio_client.get_received()
+    assert len(socketio_result) == 0  # No message sent back
+    
+    assert droneStatus.drone is None  # Drone has been reset
+    assert droneStatus.state is None  # State has been reset
+
+
+@falcon_test(pass_drone=True)
+def test_isConnectedToDrone(socketio_client: SocketIOTestClient, droneStatus):
+    """Test to see if the drone is connected when we connect"""
+    # Set drone to None and ask if we're connected
+    droneStatus.drone = None
+    socketio_client.emit("is_connected_to_drone")
+    socketio_result = socketio_client.get_received()
+    
+    assert len(socketio_result) == 1  # Only 1 response got
+    assert socketio_result[0]["args"] == [False]  # droneStatus.drone is None
+    assert socketio_result[0]["name"] == "is_connected_to_drone"  # Correct name emitted back
+
+    # Set drone and ask if we're connected
+
+

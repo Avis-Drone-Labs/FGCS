3fa6c75cd6a2bdcca74b46e0416f0b9e253ccd72
Kwashie A <104215256+Kwash67@users.noreply.github.com>
2024-06-04 16:28:16 +0100

    customized title bar and added a link to bug report

--- gcs/electron/main.ts
+++ gcs/electron/main.ts
@@ -1,6 +1,6 @@
-import { BrowserWindow, app, ipcMain, shell } from 'electron'
+import { BrowserWindow, app, ipcMain, shell, Menu } from 'electron'
 import { glob } from 'glob'
-import { ChildProcessWithoutNullStreams, spawn } from "node:child_process"
+import { ChildProcessWithoutNullStreams, spawn } from 'node:child_process'
 import fs from 'node:fs'
 import path from 'node:path'
 import os from 'os'
@@ -35,7 +35,7 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
     },
     show: false,
-    alwaysOnTop: true
+    alwaysOnTop: true,
   })
 
   win.setMenuBarVisibility(false)
@@ -43,7 +43,7 @@ function createWindow() {
   // Open links in browser, not within the electron window.
   // Note, links must have target="_blank"
   win.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url);
+    shell.openExternal(details.url)
 
     return { action: 'deny' }
   })
@@ -52,7 +52,6 @@ function createWindow() {
   win.webContents.on('did-finish-load', () => {
     win?.webContents.send('main-process-message', new Date().toLocaleString())
   })
-
 
   if (VITE_DEV_SERVER_URL) {
     win.loadURL(VITE_DEV_SERVER_URL)
@@ -68,19 +67,88 @@ function createWindow() {
     // Window starts always on top so it opens even if loading window is hid
     win?.setAlwaysOnTop(false)
   })
+
+  setMainMenu()
 }
 
-function createLoadingWindow(){
-  loadingWin= new BrowserWindow({
+function resolveHtmlPath(htmlFileName: string, hash?: string) {
+  if (process.env.NODE_ENV === 'development') {
+    const port = process.env.PORT || 5173
+    const url = new URL(`http://localhost:${port}`)
+    url.pathname = htmlFileName
+    return url.href
+  }
+  // Add hash here (if provided)
+  return `file://${path.resolve(process.env.DIST, htmlFileName)}${
+    // eslint-disable-next-line prefer-template
+    hash ? '#' + hash : ''
+  }`
+}
+
+function setMainMenu() {
+  const isMac = process.platform === 'darwin'
+
+  const template: any = [
+    // { role: 'appMenu' }
+    ...(isMac
+      ? [
+          {
+            label: app.name,
+            submenu: [
+              // { role: 'about' },
+              // { type: 'separator' },
+              {
+                label: 'About FGCS',
+                click: () => {
+                  let aboutWindow = new BrowserWindow({
+                    width: 300,
+                    height: 200,
+                    webPreferences: {
+                      nodeIntegration: true,
+                      preload: path.join(__dirname, 'preload.js'),
+                    },
+                  })
+                  aboutWindow.loadURL(`${VITE_DEV_SERVER_URL}/#/about`)
+                  aboutWindow.on('closed', function () {
+                    //aboutWindow = null;
+                  })
+                },
+              },
+              { type: 'separator' },
+              {
+                label: 'Report a bug',
+                click: async () => {
+                  await shell.openExternal(
+                    'https://github.com/Project-Falcon/FGCS/issues/new/choose',
+                  )
+                },
+              },
+              { role: 'toggleDevTools' },
+              { type: 'separator' },
+              { role: 'quit' },
+            ],
+          },
+        ]
+      : []),
+  ]
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+}
+
+function createLoadingWindow() {
+  loadingWin = new BrowserWindow({
     frame: false,
     transparent: true,
     center: true,
-  });
+  })
 
   // Resize and center window
-  loadingWin.loadFile(path.join(process.env.VITE_PUBLIC, 'window_loading_icon.svg'))
-  loadingWin.setSize(300, 300, true);
-  loadingWin.center();
+  loadingWin.loadFile(
+    path.join(process.env.VITE_PUBLIC, 'window_loading_icon.svg'),
+  )
+  loadingWin.setSize(300, 300, true)
+  loadingWin.center()
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -95,7 +163,7 @@ app.on('window-all-closed', () => {
   console.log('Killing backend')
   // kill any processes with the name "fgcs_backend.exe"
   // Windows
-  spawn('taskkill /f /im fgcs_backend.exe', {shell:true})
+  spawn('taskkill /f /im fgcs_backend.exe', { shell: true })
 })
 
 app.on('activate', () => {
@@ -110,36 +178,43 @@ app.whenReady().then(() => {
   createLoadingWindow()
   ipcMain.handle('fla:open-file', openFile)
   ipcMain.handle('fla:get-fgcs-logs', async () => {
-    const fgcsLogsPath = path.join(os.homedir(), 'FGCS','logs')
+    const fgcsLogsPath = path.join(os.homedir(), 'FGCS', 'logs')
     try {
-      const fgcsLogs = await glob(path.join(fgcsLogsPath, '*.ftlog'), {nodir: true, windowsPathsNoEscape:true}) // Get a list of .ftlog files
+      const fgcsLogs = await glob(path.join(fgcsLogsPath, '*.ftlog'), {
+        nodir: true,
+        windowsPathsNoEscape: true,
+      }) // Get a list of .ftlog files
       if (!Array.isArray(fgcsLogs)) {
-        throw new Error(`Expected fgcsLogs to be an array, but got ${typeof fgcsLogs}`);
+        throw new Error(
+          `Expected fgcsLogs to be an array, but got ${typeof fgcsLogs}`,
+        )
       }
       const slicedFgcsLogs = fgcsLogs.slice(0, 20) // Only return the last 20 logs
-  
+
       return slicedFgcsLogs.map((logPath) => {
         const logName = path.basename(logPath, '.ftlog')
         const fileStats = fs.statSync(logPath)
         return {
           name: logName,
           path: logPath,
-          size: fileStats.size
+          size: fileStats.size,
         }
       })
     } catch (error) {
-      return [];
+      return []
     }
   })
-  ipcMain.handle('app:get-node-env', () => app.isPackaged ? 'production' : 'development')
+  ipcMain.handle('app:get-node-env', () =>
+    app.isPackaged ? 'production' : 'development',
+  )
   ipcMain.handle('app:get-version', () => app.getVersion())
 
   if (app.isPackaged && pythonBackend === null) {
     console.log('Starting backend')
-    pythonBackend = spawn("extras/fgcs_backend.exe");
+    pythonBackend = spawn('extras/fgcs_backend.exe')
 
     pythonBackend.on('error', () => {
-      console.error('Failed to start backend.');
+      console.error('Failed to start backend.')
     })
   }
 

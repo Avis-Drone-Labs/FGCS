d63d3dcf46f544a858fdebbc01f6cd9a67d66f03
ShadabMir <142747276+ShadabMir@users.noreply.github.com>
2024-06-14 14:05:56 +0100

    fixed mypy error

--- radio/app/controllers/frameController.py
+++ radio/app/controllers/frameController.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from radio.app.drone import Drone
+    from app.drone import Drone
 
 
 class FrameController:

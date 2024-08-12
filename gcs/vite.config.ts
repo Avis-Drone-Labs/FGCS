0d880d4d293d1767b0f88d2d331fd1a006507b30
Julian Jones <julianjones663@gmail.com>
2024-08-12 20:32:21 +0100

    added new test mode to package.json

--- gcs/vite.config.ts
+++ gcs/vite.config.ts
@@ -7,19 +7,19 @@ import electron from 'vite-plugin-electron/simple'
 export default defineConfig({
   plugins: [
     react(),
-    // electron({
-    //   main: {
-    //     // Shortcut of `build.lib.entry`.
-    //     entry: 'electron/main.ts',
-    //   },
-    //   preload: {
-    //     // Shortcut of `build.rollupOptions.input`.
-    //     // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-    //     input: path.join(__dirname, 'electron/preload.js'),
-    //   },
-    //   // Ployfill the Electron and Node.js built-in modules for Renderer process.
-    //   // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-    //   renderer: {},
-    // }),
+    electron({
+      main: {
+        // Shortcut of `build.lib.entry`.
+        entry: 'electron/main.ts',
+      },
+      preload: {
+        // Shortcut of `build.rollupOptions.input`.
+        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+        input: path.join(__dirname, 'electron/preload.js'),
+      },
+      // Ployfill the Electron and Node.js built-in modules for Renderer process.
+      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+      renderer: {},
+    }),
   ],
 })

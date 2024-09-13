abba265837499cb75ee3c5fa1b1fc8e21d9c55f2
Julian Jones <julianjones663@gmail.com>
2024-09-13 13:55:33 +0100

    fixed ruff

--- radio/app.py
+++ radio/app.py
@@ -5,7 +5,7 @@ from app import create_app, socketio
 app = create_app(debug=True)
 
 if __name__ == "__main__":
-    port = os.getenv("PORT") if os.getenv("PORT") != None else 4237
+    port = os.getenv("PORT") if os.getenv("PORT") is not None else 4237
     print("Starting backend")
     socketio.run(app, allow_unsafe_werkzeug=True, port=port)
     if droneStatus.drone:

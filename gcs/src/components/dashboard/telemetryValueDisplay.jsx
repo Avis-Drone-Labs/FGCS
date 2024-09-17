c081bf148f9a72426fd905a3fed4d0fc06ac3677
Joe Paton <joantpat@gmail.com>
2024-09-17 12:50:46 +0100

    Added responsive font size

--- gcs/src/components/dashboard/telemetryValueDisplay.jsx
+++ gcs/src/components/dashboard/telemetryValueDisplay.jsx
@@ -1,6 +1,6 @@
-export default function TelemetryValueDisplay({ title, value }) {
+export default function TelemetryValueDisplay({ title, value, fs }) {
   return (
-    <p className='font-bold text-xl'>
+    <p className='font-bold' style={{fontSize: `${fs}rem`, lineHeight: '1.75rem'}}>
       {title} <br /> {value}
     </p>
   )

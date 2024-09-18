e0eefe2e201c6033788627147cf0d1a208119658
Kwashie A. <104215256+Kwash67@users.noreply.github.com>
2024-09-18 20:27:08 +0100

    Alpha 0.1.7/332 fix titlebar for mac (#340)
    * swapped
    
    * hid titlebar if on mac
    
    * added back bar lol
    
    * fixing titlebar for mac
    
    * fixing titlebar for mac
    
    * fixed glitching toolbar
    
    * refactored to use conditional rendering and fstrings
    
    ---------
    
    Co-authored-by: Julian Jones <julianjones663@gmail.com>

--- gcs/src/components/toolbar/toolbar.jsx
+++ gcs/src/components/toolbar/toolbar.jsx
@@ -6,7 +6,7 @@
 */
 
 // Native Imports
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Custom Imports
 import { MaximizeIcon, MinimizeIcon, CloseIcon } from "./icons.jsx";
@@ -15,37 +15,54 @@ import ViewMenu from "./menus/view.jsx";
 
 export default function Toolbar() {
   const [areMenusActive, setMenusActive] = useState(false);
+  const [isMac, setIsMac] = useState(false);
+
+  useEffect(() => {
+    window.ipcRenderer.invoke("isMac").then((result) => {
+      setIsMac(result)
+    })
+  }, [])
 
   return (
     <>
-      <div className="flex flex-row items-center justify-between bg-falcongrey-100 h-8 allow-drag">
+      <div className={`flex flex-row items-center justify-between bg-falcongrey-100 h-8 allow-drag ${isMac && "flex-row-reverse pr-2"}`}>
         {/* Logo and Menu Items */}
         <div className="pl-4 flex flex-row items-center h-full no-drag text-sm">
           {/* Icon */}
           <img src="titlebar_logo.svg" className="w-auto h-2 pr-2 object-contain allow-drag" />
 
           {/* Menu Links */}
-          <FileMenu areMenusActive={areMenusActive} setMenusActive={setMenusActive} />
-          <ViewMenu areMenusActive={areMenusActive} setMenusActive={setMenusActive} />
+          {!isMac && 
+            <>
+              <div>
+                <FileMenu areMenusActive={areMenusActive} setMenusActive={setMenusActive} />
+              </div>
+              <div>
+                <ViewMenu areMenusActive={areMenusActive} setMenusActive={setMenusActive} />
+              </div>
+            </>
+          }
         </div>
 
         {/* Window actions (close, minimise, maximise) */}
-        <div className="flex flex-row items-center h-full">
-          {/* Minimise */}
-          <div title="Minimise" className="px-3 flex items-center h-full no-drag cursor-pointer hover:bg-falcongrey-80" onClick={() => {window.ipcRenderer.send('minimise', [])}} label="Minimise">
-            <MinimizeIcon className="stroke-slate-400" />
-          </div>
+        {!isMac && 
+          <div className="flex flex-row items-center h-full">
+            {/* Minimise */}
+            <div title="Minimise" className="px-3 flex items-center h-full no-drag cursor-pointer hover:bg-falcongrey-80" onClick={() => {window.ipcRenderer.send('minimise', [])}} label="Minimise">
+              <MinimizeIcon className="stroke-slate-400" />
+            </div>
 
-          {/* Maximise */}
-          <div title="Maximise" className="px-3 flex items-center h-full no-drag cursor-pointer hover:bg-falcongrey-80" onClick={() => {window.ipcRenderer.send('maximise', [])}} label="Maximise">
-            <MaximizeIcon className="stroke-slate-400" />
-          </div>
+            {/* Maximise */}
+            <div title="Maximise" className="px-3 flex items-center h-full no-drag cursor-pointer hover:bg-falcongrey-80" onClick={() => {window.ipcRenderer.send('maximise', [])}} label="Maximise">
+              <MaximizeIcon className="stroke-slate-400" />
+            </div>
 
-          {/* Close */}
-          <div title="Close" className="px-3 flex items-center h-full no-drag cursor-pointer group hover:bg-red-500" onClick={() => {window.ipcRenderer.send('close', [])}} label="Close">
-            <CloseIcon className="stroke-slate-400 group-hover:stroke-white" />
+            {/* Close */}
+            <div title="Close" className="px-3 flex items-center h-full no-drag cursor-pointer group hover:bg-red-500" onClick={() => {window.ipcRenderer.send('close', [])}} label="Close">
+              <CloseIcon className="stroke-slate-400 group-hover:stroke-white" />
+            </div>
           </div>
-        </div>
+        }
       </div>
     </>
   )

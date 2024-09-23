d5d29dbb26809da4aec7c808f06c43da53f64c54
Julian Jones <julianjones663@gmail.com>
2024-09-23 23:52:48 +0100

    Updated to use env correctly

--- radio/app.py
+++ radio/app.py
@@ -1,12 +1,23 @@
 import os
 import app.droneStatus as droneStatus
 from app import create_app, socketio
+from pathlib import Path
+from dotenv import load_dotenv
 
 app = create_app(debug=True)
 
 if __name__ == "__main__":
-    port = os.getenv("PORT") if os.getenv("PORT") is not None else 4237
-    print("Starting backend")
+    print("Loading dotenv.")
+    env_path = Path("../gcs/.env")
+    load_dotenv(dotenv_path=env_path)
+    port = (
+        os.getenv("VITE_BACKEND_URL").split(":")[-1]
+        if os.getenv("VITE_BACKEND_URL") is not None
+        else 4237
+    )
+
+    print("Starting backend.")
     socketio.run(app, allow_unsafe_werkzeug=True, port=port)
     if droneStatus.drone:
         droneStatus.drone.close()
+        print("Backend closed.")

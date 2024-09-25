d4e55442ee7c15a56afd4cc3e52676a5c3277a7f
Julian Jones <julianjones663@gmail.com>
2024-09-25 18:56:44 +0100

    updated mantine theme to use falcongrey

--- gcs/src/components/customMantineTheme.jsx
+++ gcs/src/components/customMantineTheme.jsx
@@ -1,20 +1,30 @@
+/*
+  Custom mantine colour theme using tailwind colours
+*/
+
+// 3rd Party imports 
 import { createTheme } from '@mantine/core';
 
+// Styling imports
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../tailwind.config.js'
+
+const tailwindColors = resolveConfig(tailwindConfig).theme.colors
 export const CustomMantineTheme = createTheme({
   fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;',
   lineHeights: "1.5",
   colors: {
     dark: [
-      '#d5d7e0',
-      '#acaebf',
-      '#8c8fa3',
-      '#666980',
-      '#4d4f66',
-      '#34354a',
-      '#2b2c3d',
-      '#1d1e30',
-      '#0c0d21',
-      '#01010a',
+      tailwindColors.falcongrey[100],
+      tailwindColors.falcongrey[200],
+      tailwindColors.falcongrey[300],
+      tailwindColors.falcongrey[400],
+      tailwindColors.falcongrey[500],
+      tailwindColors.falcongrey[600],
+      tailwindColors.falcongrey[700],
+      tailwindColors.falcongrey[800],
+      tailwindColors.falcongrey[900],
+      tailwindColors.falcongrey[950],
     ]
   }
 });

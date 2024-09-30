159053e30e2ae44db81b6a220b6ef54013a79b7e
Joe <joantpat@gmail.com>
2024-09-30 02:37:26 +0100

    Added final state test

--- radio/tests/test_states.py
+++ radio/tests/test_states.py
@@ -35,6 +35,12 @@ def test_setState(socketio_client: SocketIOTestClient, droneStatus) -> None:
 
     droneStatus.drone.message_listeners = {}
 
+    socketio_client.emit("set_state", {"state": "config"})
+    assert len(socketio_client.get_received()) == 0
+    assert len(droneStatus.drone.message_listeners) == 0
+
+    droneStatus.drone.message_listeners = {}
+
     socketio_client.emit("set_state", {"state": "config.flight_modes"})
     assert len(socketio_client.get_received()) == 0
     assert len(droneStatus.drone.message_listeners) == 2
@@ -47,7 +53,8 @@ def test_setState(socketio_client: SocketIOTestClient, droneStatus) -> None:
 
     droneStatus.drone.message_listeners = {}
 
-    # Test
+    # Test switch to params when parameters are not loaded
+    droneStatus.drone.paramsController.params = []
     socketio_client.emit("set_state", {"state": "params"})
     while (recieved := socketio_client.get_received()[-1])[
         "name"
@@ -55,6 +62,12 @@ def test_setState(socketio_client: SocketIOTestClient, droneStatus) -> None:
         assert recieved["args"][0]["total_number_of_params"] == 1400
         assert recieved["args"][0]["current_param_index"] < 1400
 
+    assert recieved["name"] == "params"
+    assert len(recieved["args"][0]) == 1400
+    assert len(droneStatus.drone.message_listeners) == 0
+
+    # Test switching to params when parameters are already loaded
+    socketio_client.emit("set_state", {"state": "params"})
     assert recieved["name"] == "params"
     assert len(recieved["args"][0]) == 1400
     assert len(droneStatus.drone.message_listeners) == 0

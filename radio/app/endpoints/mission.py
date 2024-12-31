15748b716925d210c7e02a2331466d9ba2c53f6a
Kush Makkapati <kush.makkapati@icloud.com>
2024-12-31 03:54:53 +0000

    Add list of mission commands to filter out before sending to frontend

--- radio/app/endpoints/mission.py
+++ radio/app/endpoints/mission.py
@@ -1,8 +1,61 @@
+from pymavlink import mavutil
 from typing_extensions import TypedDict
 
 import app.droneStatus as droneStatus
 from app import logger, socketio
 from app.utils import notConnectedError
+
+# Filter out specific mission items, e.g. takeoff command
+filter_mission_commands_list = [
+    mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH,
+    mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
+    mavutil.mavlink.MAV_CMD_NAV_LAND_LOCAL,
+    mavutil.mavlink.MAV_CMD_NAV_TAKEOFF_LOCAL,
+    mavutil.mavlink.MAV_CMD_NAV_TAKEOFF_LOCAL,
+    mavutil.mavlink.MAV_CMD_NAV_ALTITUDE_WAIT,
+    mavutil.mavlink.MAV_CMD_NAV_CONTINUE_AND_CHANGE_ALT,
+    mavutil.mavlink.MAV_CMD_NAV_DELAY,
+    mavutil.mavlink.MAV_CMD_CONDITION_DISTANCE,
+    mavutil.mavlink.MAV_CMD_DO_AUX_FUNCTION,
+    mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED,
+    mavutil.mavlink.MAV_CMD_DO_ENGINE_CONTROL,
+    mavutil.mavlink.MAV_CMD_DO_VTOL_TRANSITION,
+    mavutil.mavlink.MAV_CMD_DO_SET_SERVO,
+    mavutil.mavlink.MAV_CMD_DO_SET_RELAY,
+    mavutil.mavlink.MAV_CMD_DO_REPEAT_SERVO,
+    mavutil.mavlink.MAV_CMD_DO_REPEAT_RELAY,
+    mavutil.mavlink.MAV_CMD_DO_DIGICAM_CONFIGURE,
+    mavutil.mavlink.MAV_CMD_DO_DIGICAM_CONTROL,
+    mavutil.mavlink.MAV_CMD_DO_SET_CAM_TRIGG_DIST,
+    mavutil.mavlink.MAV_CMD_DO_GIMBAL_MANAGER_PITCHYAW,
+    mavutil.mavlink.MAV_CMD_DO_JUMP,
+    mavutil.mavlink.MAV_CMD_JUMP_TAG,
+    mavutil.mavlink.MAV_CMD_DO_JUMP_TAG,
+    mavutil.mavlink.MAV_CMD_DO_MOUNT_CONTROL,
+    mavutil.mavlink.MAV_CMD_DO_INVERTED_FLIGHT,
+    mavutil.mavlink.MAV_CMD_DO_FENCE_ENABLE,
+    mavutil.mavlink.MAV_CMD_DO_AUTOTUNE_ENABLE,
+    mavutil.mavlink.MAV_CMD_DO_SET_RESUME_REPEAT_DIST,
+    mavutil.mavlink.MAV_CMD_STORAGE_FORMAT,
+    mavutil.mavlink.MAV_CMD_NAV_GUIDED_ENABLE,
+    mavutil.mavlink.MAV_CMD_MISSION_START,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    mavutil.mavlink.MAV_CMD_CONDITION_DELAY,
+    mavutil.mavlink.MAV_CMD_CONDITION_DISTANCE,
+    mavutil.mavlink.MAV_CMD_CONDITION_YAW,
+    mavutil.mavlink.MAV_CMD_DO_AUX_FUNCTION,
+    mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED,
+    mavutil.mavlink.MAV_CMD_DO_PARACHUTE,
+    mavutil.mavlink.MAV_CMD_DO_GRIPPER,
+    mavutil.mavlink.MAV_CMD_DO_GUIDED_LIMITS,
+    mavutil.mavlink.MAV_CMD_DO_SET_RESUME_REPEAT_DIST,
+    mavutil.mavlink.MAV_CMD_DO_FENCE_ENABLE,
+    mavutil.mavlink.MAV_CMD_DO_WINCH,
+    mavutil.mavlink.MAV_CMD_DO_FOLLOW,
+    mavutil.mavlink.MAV_CMD_DO_FOLLOW_REPOSITION,
+    mavutil.mavlink.MAV_CMD_CONDITION_DELAY,
+    mavutil.mavlink.MAV_CMD_CONDITION_YAW,
+]
 
 
 class ControlMissionType(TypedDict):
@@ -37,9 +90,11 @@ def getCurrentMission() -> None:
         item.to_dict() for item in droneStatus.drone.missionController.rally_items
     ]
 
-    # Filter out specific mission items, e.g. takeoff command
     mission_items_filtered = list(
-        filter(lambda item: item.get("command") != 22, mission_items)
+        filter(
+            lambda item: item.get("command") not in filter_mission_commands_list,
+            mission_items,
+        )
     )
 
     socketio.emit(

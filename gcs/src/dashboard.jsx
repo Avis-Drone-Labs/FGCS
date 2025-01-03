708f0d619a09c64b330b609bfe172dd0d2cf914d
Julian Jones <37962677+NexInfinite@users.noreply.github.com>
2025-01-03 03:01:49 +0000

    Dashboard refactor/457 move telemetry information (#458)
    * moved into new component
    
    * formatted
    
    * added comments

--- gcs/src/dashboard.jsx
+++ gcs/src/dashboard.jsx
@@ -56,16 +56,13 @@ import { socket } from './helpers/socket'
 
 // Custom component
 import useSound from 'use-sound'
-import {
-  AttitudeIndicator,
-  HeadingIndicator,
-} from './components/dashboard/indicator'
 import MapSection from './components/dashboard/map'
 import StatusBar, { StatusSection } from './components/dashboard/statusBar'
 import StatusMessages from './components/dashboard/statusMessages'
 import DashboardDataModal from './components/dashboardDataModal'
 import FloatingToolbar from './components/dashboard/floatingToolbar'
 import ResizableInfoBox from './components/dashboard/resizableInfoBox'
+import TelemetrySection from './components/dashboard/telemetry'
 import Layout from './components/layout'
 
 // Tailwind styling
@@ -76,7 +73,6 @@ const tailwindColors = resolveConfig(tailwindConfig).theme.colors
 // Sounds
 import armSound from './assets/sounds/armed.mp3'
 import disarmSound from './assets/sounds/disarmed.mp3'
-import TelemetryValueDisplay from './components/dashboard/telemetryValueDisplay'
 import { defaultDataMessages } from './helpers/dashboardDefaultDataMessages'
 import { dataFormatters } from './helpers/dataFormatters'
 
@@ -519,159 +515,25 @@ export default function Dashboard() {
           calcBigTextFontSize={calcBigTextFontSize}
         >
           {/* Telemetry Information */}
-          <div>
-            {/* Information above indicators */}
-            <div className='flex flex-col items-center space-y-2'>
-              {getIsArmed() ? (
-                <p className='font-bold text-falconred'>ARMED</p>
-              ) : (
-                <>
-                  <p className='font-bold'>DISARMED</p>
-                  {prearmEnabled() ? (
-                    <p className='text-green-500'>Prearm: Enabled</p>
-                  ) : (
-                    <p className='font-bold text-falconred'>Prearm: Disabled</p>
-                  )}
-                </>
-              )}
-              <div className='flex flex-row space-x-6'>
-                <p>{MAV_STATE[heartbeatData.system_status]}</p>
-                <p>{getFlightMode()}</p>
-              </div>
-            </div>
-
-            <div className='flex items-center flex-col justify-center justify-evenly @xl:flex-row'>
-              {/* Attitude Indicator */}
-              <div
-                className='flex flex-row items-center justify-center'
-                style={{
-                  paddingTop: `${calcIndicatorPadding()}px`,
-                  paddingBottom: `${calcIndicatorPadding()}px`,
-                }}
-              >
-                <div className='flex flex-col items-center justify-center space-y-4 text-center min-w-14'>
-                  <p className='text-sm text-center'>ms&#8315;&#185;</p>
-                  <TelemetryValueDisplay
-                    title='AS'
-                    value={(telemetryData.airspeed
-                      ? telemetryData.airspeed
-                      : 0
-                    ).toFixed(2)}
-                    fs={telemetryFontSize}
-                  />
-                  <TelemetryValueDisplay
-                    title='GS'
-                    value={(telemetryData.groundspeed
-                      ? telemetryData.groundspeed
-                      : 0
-                    ).toFixed(2)}
-                    fs={telemetryFontSize}
-                  />
-                </div>
-                <AttitudeIndicator
-                  roll={attitudeData.roll * (180 / Math.PI)}
-                  pitch={attitudeData.pitch * (180 / Math.PI)}
-                  size={`${calcIndicatorSize()}px`}
-                />
-                <div className='flex flex-col items-center justify-center space-y-4 text-center min-w-14'>
-                  <p className='text-sm text-center'>m</p>
-                  <TelemetryValueDisplay
-                    title='AMSL'
-                    value={(gpsData.alt ? gpsData.alt / 1000 : 0).toFixed(2)}
-                    fs={telemetryFontSize}
-                  />
-                  <TelemetryValueDisplay
-                    title='AREL'
-                    value={(gpsData.relative_alt
-                      ? gpsData.relative_alt / 1000
-                      : 0
-                    ).toFixed(2)}
-                    fs={telemetryFontSize}
-                  />
-                </div>
-              </div>
-
-              {/* Heading Indicator */}
-              <div
-                className='flex flex-row items-center justify-center'
-                style={{
-                  paddingTop: `${calcIndicatorPadding()}px`,
-                  paddingBottom: `${calcIndicatorPadding()}px`,
-                }}
-              >
-                <div className='flex flex-col items-center justify-center space-y-4 text-center min-w-14'>
-                  <p className='text-sm text-center'>deg &#176;</p>
-                  <TelemetryValueDisplay
-                    title='HDG'
-                    value={(gpsData.hdg ? gpsData.hdg / 100 : 0).toFixed(2)}
-                    fs={telemetryFontSize}
-                  />
-                  <TelemetryValueDisplay
-                    title='YAW'
-                    value={(attitudeData.yaw
-                      ? attitudeData.yaw * (180 / Math.PI)
-                      : 0
-                    ).toFixed(2)}
-                    fs={telemetryFontSize}
-                  />
-                </div>
-                <HeadingIndicator
-                  heading={gpsData.hdg ? gpsData.hdg / 100 : 0}
-                  size={`${calcIndicatorSize()}px`}
-                />
-                <div
-                  className='flex flex-col items-center justify-center space-y-4 text-center min-w-14'
-                  ref={sideBarRef}
-                >
-                  <p className='text-sm'>m</p>
-                  <TelemetryValueDisplay
-                    title='WP'
-                    value={(navControllerOutputData.wp_dist
-                      ? navControllerOutputData.wp_dist
-                      : 0
-                    ).toFixed(2)}
-                    fs={telemetryFontSize}
-                  />
-                  {/* TOOD: Implement distance to home */}
-                  <TelemetryValueDisplay
-                    title='HOME'
-                    value={(0).toFixed(2)}
-                    fs={telemetryFontSize}
-                  />
-                </div>
-              </div>
-            </div>
-
-            {/* Batter information */}
-            <div className='flex flex-col items-center'>
-              <p>BATTERY</p>
-              <div className='flex flex-row space-x-4'>
-                <p className='font-bold text-xl'>
-                  {(batteryData.voltages
-                    ? batteryData.voltages[0] / 1000
-                    : 0
-                  ).toFixed(2)}
-                  V
-                </p>
-                <p className='font-bold text-xl'>
-                  {(batteryData.current_battery
-                    ? batteryData.current_battery / 100
-                    : 0
-                  ).toFixed(2)}
-                  A
-                </p>
-                <p className='font-bold text-xl'>
-                  {batteryData.battery_remaining
-                    ? batteryData.battery_remaining
-                    : 0}
-                  %
-                </p>
-              </div>
-            </div>
-          </div>
+          <TelemetrySection
+            getIsArmed={getIsArmed}
+            prearmEnabled={prearmEnabled}
+            calcIndicatorSize={calcIndicatorSize}
+            calcIndicatorPadding={calcIndicatorPadding}
+            getFlightMode={getFlightMode}
+            telemetryData={telemetryData}
+            telemetryFontSize={telemetryFontSize}
+            attitudeData={attitudeData}
+            gpsData={gpsData}
+            sideBarRef={sideBarRef}
+            navControllerOutputData={navControllerOutputData}
+            batteryData={batteryData}
+            systemStatus={MAV_STATE[heartbeatData.system_status]}
+          />
 
           <Divider className='my-2' />
 
+          {/* Actions */}
           <Tabs defaultValue='data'>
             <Tabs.List grow>
               <Tabs.Tab value='data'>Data</Tabs.Tab>

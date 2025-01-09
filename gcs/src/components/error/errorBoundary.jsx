8ceda3e8a489430c08879430530f98307a431b3f
Julian Jones <julianjones663@gmail.com>
2025-01-09 18:36:58 +0000

    formatted

--- gcs/src/components/error/errorBoundary.jsx
+++ gcs/src/components/error/errorBoundary.jsx
@@ -3,32 +3,31 @@
 */
 
 // Local Imports
-import React from "react";
+import React from "react"
 
 // 3rd Party Imports
-import { Button } from "@mantine/core";
+import { Button } from "@mantine/core"
 
 export function ErrorComponent() {
   return (
     <div className="flex flex-col w-full h-full items-center justify-center text-center text-xl gap-y-2">
-      <h1 className="font-bold text-4xl text-falconred-700">We've ran into an issue!</h1>
+      <h1 className="font-bold text-4xl text-falconred-700">
+        We've ran into an issue!
+      </h1>
 
       <div>
         <p>
-          Please report this{' '}
-          <a 
+          Please report this{" "}
+          <a
             href="https://github.com/Avis-Drone-Labs/FGCS/issues/new?assignees=&labels=bug&projects=&template=bug_report.md&title=%5BBUG%5D"
             target="_blank"
             className="underline text-falconred-700"
           >
             here
-          </a>
-          {' '}
+          </a>{" "}
           so we can fix it as soon as possible!
         </p>
-        <p>
-          To get back to what you were doing, refresh or click below.
-        </p>
+        <p>To get back to what you were doing, refresh or click below.</p>
       </div>
 
       <Button
@@ -46,23 +45,23 @@ export function ErrorComponent() {
 
 export class ErrorBoundary extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { hasError: false };
+    super(props)
+    this.state = { hasError: false }
   }
 
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
+  static getDerivedStateFromError() {
+    return { hasError: true }
   }
 
   componentDidCatch(error, errorInfo) {
-    console.log(error, errorInfo);
+    console.log(error, errorInfo)
   }
 
   render() {
     if (this.state.hasError) {
-      return <ErrorComponent></ErrorComponent>;
+      return <ErrorComponent></ErrorComponent>
     }
 
-    return this.props.children; 
+    return this.props.children
   }
 }

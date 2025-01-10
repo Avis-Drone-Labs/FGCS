ff9b87f5707c0885acb6d4c122c5468904a1d259
David Gasinski <dgasinski1@sheffield.ac.uk>
2025-01-10 11:50:54 +0000

    fixed, moved exported functions that arent components into separate files

--- gcs/src/helpers/dataFormatters.js
+++ gcs/src/helpers/dataFormatters.js
@@ -2,6 +2,11 @@ function radToDeg(val) {
   return (val * 180) / Math.PI
 }
 
+// Convert coordinates from mavlink into gps coordinates
+export function intToCoord(val) {
+  return val * 1e-7
+}
+
 export const dataFormatters = {
   "ATTITUDE.pitch": radToDeg,
   "ATTITUDE.roll": radToDeg,

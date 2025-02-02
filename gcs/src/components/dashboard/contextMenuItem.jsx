52bde7118b73c7623b5443a23388e1056a04d55e
Ben Gilbert <bensgilbert@outlook.com>
2025-02-02 17:33:46 +0000

    removed unnecessary default param
    I really don't know why I put it there in the first place???

--- gcs/src/components/dashboard/contextMenuItem.jsx
+++ gcs/src/components/dashboard/contextMenuItem.jsx
@@ -1,4 +1,4 @@
-export default function ContextMenuItem({ text, onClick = undefined }) {
+export default function ContextMenuItem({ text, onClick }) {
   return (
     <div
       className={`${onClick ? "hover:bg-falcongrey-800 hover:cursor-pointer" : ""} py-1 px-6 rounded`}

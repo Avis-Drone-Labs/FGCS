d28bd5a3a35564b5b280249bee7025dee96955b7
Ben Gilbert <bensgilbert@outlook.com>
2025-02-17 17:26:59 +0000

    formatting

--- gcs/src/components/dashboard/map.jsx
+++ gcs/src/components/dashboard/map.jsx
@@ -524,20 +524,21 @@ export default function MapSection({
             className="absolute bg-falcongrey-700 rounded-md p-1"
             style={{ top: points.y, left: points.x }}
           >
-            <ContextMenuItem
-              text={`${clickedGpsCoords.lat.toFixed(coordsFractionDigits)}, ${clickedGpsCoords.lng.toFixed(coordsFractionDigits)}`}
-            ></ContextMenuItem>
+            <ContextMenuItem onClick={open}>Fly to here</ContextMenuItem>
             <Divider className="my-1" />
-            <ContextMenuItem text="Fly to here" onClick={open} />
             <ContextMenuItem
-              text="Copy coords"
               onClick={() => {
                 clipboard.copy(
                   `${clickedGpsCoords.lat}, ${clickedGpsCoords.lng}`,
                 )
                 showNotification("Copied to clipboard")
               }}
-            />
+            >
+              <div className="w-full flex justify-between gap-2">
+                <p>{clickedGpsCoords.lat.toFixed(coordsFractionDigits)}, {clickedGpsCoords.lng.toFixed(coordsFractionDigits)}</p>
+                <svg className="relative -right-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M9 18q-.825 0-1.412-.587T7 16V4q0-.825.588-1.412T9 2h9q.825 0 1.413.588T20 4v12q0 .825-.587 1.413T18 18zm0-2h9V4H9zm-4 6q-.825 0-1.412-.587T3 20V7q0-.425.288-.712T4 6t.713.288T5 7v13h10q.425 0 .713.288T16 21t-.288.713T15 22zm4-6V4z" /></svg>
+              </div>
+            </ContextMenuItem>
           </div>
         )}
       </Map>

9ea25236ecc90e0cb6c371adff2657fc82fdc07e
Joe <joantpat@gmail.com>
2025-03-19 19:49:24 +0000

    Alpha 0.1.9/517 popout camera feed from dashboard (#541)
    * Moved maincontent to separate component to make fast refresh work
    
    * Added webcam picture-in-picture window
    
    * Create API for webcam window
    
    * Create webcam route
    
    * Update camera tab section to allow for camera popout
    
    * Fixed bug with multiple resize handlers stacking
    
    * Added nullify webcam window on close
    
    * Fixed resize issues
    
    * Added some coments
    
    * Fixed buge where camera breaks when no video stream
    
    * Fixed webcam being always on in the background
    
    * Explicitly prevent fullscreen
    
    * Oops I removed fullscreen on the wrong window
    
    * improved styling
    
    * formatted
    
    * Added errorboundaryfallback from #306382b
    
    * Fixed import error
    
    ---------
    
    Co-authored-by: Julian Jones <julianjones663@gmail.com>

--- gcs/src/components/dashboard/tabsSectionTabs/cameraTabsSection.jsx
+++ gcs/src/components/dashboard/tabsSectionTabs/cameraTabsSection.jsx
@@ -4,7 +4,7 @@
  */
 
 // Native
-import { useCallback, useState, useEffect } from "react"
+import { useCallback, useState, useEffect, useRef } from "react"
 
 // Mantine
 import { useSessionStorage } from "@mantine/hooks"
@@ -12,6 +12,7 @@ import { Tabs, Select } from "@mantine/core"
 
 // Helper
 import Webcam from "react-webcam"
+import { IconExternalLink, IconVideoOff } from "@tabler/icons-react"
 
 export default function CameraTabsSection({ tabPadding }) {
   // Camera devices
@@ -19,7 +20,16 @@ export default function CameraTabsSection({ tabPadding }) {
     key: "deviceId",
     defaultValue: null,
   })
+
+  window.ipcRenderer.onCameraWindowClose(() => setPictureInPicture(false))
+
+  // Ref used to get video capture stream to send to new electron window
+  const videoRef = useRef(null)
   const [devices, setDevices] = useState([])
+
+  const [streamLoaded, setStreamLoaded] = useState(false)
+  const [invalidStream, setInvalidStream] = useState(false)
+  const [pictureInPicture, setPictureInPicture] = useState(false)
 
   const handleDevices = useCallback(
     (mediaDevices) =>
@@ -31,9 +41,29 @@ export default function CameraTabsSection({ tabPadding }) {
     navigator.mediaDevices.enumerateDevices().then(handleDevices)
   }, [handleDevices])
 
+  function toggleWebcamPopout() {
+    const streamTrack = videoRef.current.video.srcObject.getTracks()[0]
+    const streamAspect =
+      streamTrack.getSettings().width / streamTrack.getSettings().height
+
+    pictureInPicture
+      ? window.ipcRenderer.closeWebcamWindow()
+      : window.ipcRenderer.openWebcamWindow(
+          deviceId,
+          streamTrack.label,
+          streamAspect,
+        )
+    setPictureInPicture(!pictureInPicture)
+  }
+
+  function onStreamLoaded() {
+    setInvalidStream(false)
+    setStreamLoaded(true)
+  }
+
   return (
     <Tabs.Panel value="camera">
-      <div className={`flex flex-col gap-4 text-xl ${tabPadding}`}>
+      <div className={`flex flex-col gap-4 text-xl ${tabPadding} items-center`}>
         <Select
           placeholder="Select camera input"
           data={devices.map((device) => {
@@ -41,9 +71,44 @@ export default function CameraTabsSection({ tabPadding }) {
           })}
           value={deviceId}
           onChange={setDeviceId}
+          className={`w-[100%] max-w-[350px] @xl:max-w-[640px]`}
         />
         {deviceId !== null && (
-          <Webcam audio={false} videoConstraints={{ deviceId: deviceId }} />
+          <div className="relative">
+            <Webcam
+              onUserMedia={() => onStreamLoaded()}
+              ref={videoRef}
+              audio={false}
+              videoConstraints={{ deviceId: deviceId }}
+              className="max-w-[350px] w-[100%] @xl:max-w-[640px]"
+              onUserMediaError={() => setInvalidStream(true)}
+            />
+            {/* Overlay black background instead of conditionally rendering webcam to prevent reloading stream */}
+            {pictureInPicture && (
+              <div className="absolute top-0 right-0 w-[100%] h-[100%] bg-black" />
+            )}
+
+            {/* Overlay invalid stream message if video stream failed to be created */}
+            {invalidStream && (
+              <div className="flex justify-center items-center absolute top-0 right-0 w-[100%] h-[100%] bg-falcongrey-700">
+                <div className="flex flex-col items-center h-[75%] justify-center">
+                  <IconVideoOff size={"50%"} />
+                  <p className="">No video stream available</p>
+                </div>
+              </div>
+            )}
+            {streamLoaded && !pictureInPicture && !invalidStream && (
+              <button
+                className="absolute top-2 right-2 bg-falcongrey-900/60 p-1 rounded-[0.2em]"
+                onClick={() => toggleWebcamPopout()}
+              >
+                <IconExternalLink
+                  stroke={2}
+                  className="stroke-slate-200 size-5"
+                />
+              </button>
+            )}
+          </div>
         )}
       </div>
     </Tabs.Panel>

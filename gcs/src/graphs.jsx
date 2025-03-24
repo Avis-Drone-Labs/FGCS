d28fa2cae0d50b097439a58efb7b5aebddee0654
Ben Gilbert <bensgilbert@outlook.com>
2025-03-24 21:50:11 +0000

    changed tab message (#549)

--- gcs/src/graphs.jsx
+++ gcs/src/graphs.jsx
@@ -192,7 +192,7 @@ export default function Graphs() {
           />
         </div>
       ) : (
-        <NoDroneConnected />
+        <NoDroneConnected tab="graphs" />
       )}
     </Layout>
   )

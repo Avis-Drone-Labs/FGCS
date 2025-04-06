1c0ac6bed8d0d666556a32777e16bb11d398be8f
Julian Jones <julianjones663@gmail.com>
2025-04-06 21:51:57 +0100

    added update

--- run.bash
+++ run.bash
@@ -1,2 +1,13 @@
+#!/bin/sh
+
 source radio/venv/bin/activate
-concurrently "python radio/app.py" "cd gcs && yarn dev" -n "backend,frontend" -c "red,blue"
+
+# Check for updates
+if [ -z "$1" ]; then 
+    concurrently "python radio/app.py" "cd gcs && yarn dev" -n "backend,frontend" -c "red,blue"
+else
+    cd radio
+    pip install -r requirements.txt
+    cd ../
+    concurrently "python radio/app.py" "cd gcs && yarn dev" -n "backend,frontend" -c "red,blue"
+fi

cc7f592c0946f86efef3dde2d43cbeac16f8d28c
Kush Makkapati <kush.makkapati@icloud.com>
2025-08-18 17:21:11 +0100

    Try fix playwright action

--- gcs/playwright.config.ts
+++ gcs/playwright.config.ts
@@ -76,10 +76,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'yarn dev:test',
-  //   url: 'http://127.0.0.1:5173',
-  //   reuseExistingServer: !process.env.CI,
-  //   timeout: 120 * 1000,
-  // },
+  webServer: {
+    command: 'yarn dev:test &',
+    url: 'http://127.0.0.1:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });

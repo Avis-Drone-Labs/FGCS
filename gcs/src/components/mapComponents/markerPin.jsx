3a2d2fc9229214a9af7f35d001cadf99091f92cf
Kush Makkapati <kush.makkapati@icloud.com>
2025-08-20 07:22:12 +0100

    Add styling update to marker pin

--- gcs/src/components/mapComponents/markerPin.jsx
+++ gcs/src/components/mapComponents/markerPin.jsx
@@ -23,41 +23,48 @@ const MarkerPin = React.memo(
     dragEndCallback = () => {},
   }) => {
     return (
-      <Marker
-        latitude={lat}
-        longitude={lon}
-        className={showOnTop && "z-10"}
-        draggable={draggable}
-        onDragEnd={(e) => {
-          dragEndCallback({
-            id: id,
-            x: coordToInt(e.lngLat.lat),
-            y: coordToInt(e.lngLat.lng),
-          })
+      <div
+        onContextMenu={() => {
+          console.log(`Right-click on marker with ID: ${id}`)
         }}
       >
-        <Tooltip disabled={tooltipText === null} label={tooltipText}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 48"
-            fill={colour}
-            stroke="currentColor"
-            strokeWidth="1"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="icon icon-tabler icons-tabler-outline icon-tabler-map-pin h-16 w-16 text-black"
-          >
-            <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
-            {text && (
-              <text textAnchor="middle" x="12" y="14" className="text-black">
-                {text}
-              </text>
-            )}
-          </svg>
-        </Tooltip>
-      </Marker>
+        <Marker
+          latitude={lat}
+          longitude={lon}
+          className={showOnTop && "z-10"}
+          offset={[0, -15]}
+          draggable={draggable}
+          onDragEnd={(e) => {
+            dragEndCallback({
+              id: id,
+              x: coordToInt(e.lngLat.lat),
+              y: coordToInt(e.lngLat.lng),
+            })
+          }}
+        >
+          <Tooltip disabled={tooltipText === null} label={tooltipText}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              viewBox="0 0 24 24"
+              fill={colour}
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-map-pin text-black"
+            >
+              <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+              {text && (
+                <text textAnchor="middle" x="12" y="14" className="text-black">
+                  {text}
+                </text>
+              )}
+            </svg>
+          </Tooltip>
+        </Marker>
+      </div>
     )
   },
   (prevProps, nextProps) => {

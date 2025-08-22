87977487b8ab2c4705b21293da6ca355320a72d5
Joe <joantpat@gmail.com>
2025-08-22 09:20:21 +0100

    Added logging stream handler and socket handler

--- radio/app/loggingConfig.py
+++ radio/app/loggingConfig.py
@@ -1,9 +1,34 @@
+import sys
 import logging
 
-def setup_logging() -> logging.Logger:
+from flask_socketio import SocketIO
+
+
+class SocketIOHandler(logging.Handler):
+    
+    def __init__(self, sio: SocketIO) -> None:
+        super().__init__()
+        self.socket = sio
+    
+    def emit(self, record) -> None:
+        try:
+            entry = self.format(record)
+            self.socket.emit("log", {"level": record.levelname, "msg": entry})
+        except:
+            self.handleError(record)
+
+
+def setup_logging(conn: SocketIO, debug: bool = False) -> logging.Logger:
     
     fgcs_logger = logging.getLogger("fgcs")
     
-    fgcs_logger.addHandler(
-        logging.handlers
-    )
+    fgcs_logger.setLevel(logging.DEBUG if debug else logging.INFO)
+    fgcs_logger.addHandler(SocketIOHandler(conn))
+    
+    # Stream handler on debug mode only
+    if debug:
+        fgcs_logger.addHandler(logging.StreamHandler(sys.stderr))
+    
+    flask_logger = logging.getLogger("werkzeug")
+    flask_logger.setLevel(logging.WARNING)
+    flask_logger.addHandler(SocketIOHandler(conn))

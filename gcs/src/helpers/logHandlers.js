5ba4638b333d0e2064d67cf898bbdea2099873af
Joe <joantpat@gmail.com>
2025-08-22 11:18:24 +0100

    Added two log handlers

---
+++ gcs/src/helpers/logHandlers.js
@@ -0,0 +1,10 @@
+import { ipcRenderer } from "electron";
+
+
+export function consoleLogHandler(msg) {
+    console.log(`[${msg.level}] [${msg.timestamp}] ${msg.message}`)
+}
+
+export function electronLogHandler(msg) {
+    window.ipcRenderer.pushLog(msg);
+}

928c94db0ad9f1c5eec03b6863668fa7a360b8a4
Joe <joantpat@gmail.com>
2025-08-22 11:19:26 +0100

    Add log history

---
+++ gcs/src/redux/slices/loggingSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const KEEP_LATEST_N_LOGS = 50;
+
+const loggingSlice = createSlice({
+    name: "logging",
+    initialState: {
+        logHistory: [],
+        handlers: [],
+    },
+    reducers: {
+        emitLog: (state, action) => {
+            state.logHistory.push(action.payload);
+            if (state.logHistory.length > KEEP_LATEST_N_LOGS) state.logHistory.shift();
+
+            state.handlers.forEach(handler => {
+                handler(action.payload);
+            })
+        },
+        registerHandler: (state, action) => {
+            if (action.payload in state.handlers) return;
+            state.handlers.push(action.payload);
+        }
+    },
+    selectors: {
+        selectRecentLogs: (state) => state.logHistory,
+        selectMostRecent: (state, n) => state.logHistory.slice(-n, -1)
+    },
+
+})
+
+export const {emitLog, registerHandler} = loggingSlice.actions
+export const {selectMostRecent, selectRecentLogs} = loggingSlice.selectors
+
+export default loggingSlice

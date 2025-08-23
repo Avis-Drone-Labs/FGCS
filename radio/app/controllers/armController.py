c3832a9cad5554d9d23ef35a07cf5a9db164cf3b
Joe <joantpat@gmail.com>
2025-08-23 16:20:25 +0100

    Updated logging methods

--- radio/app/controllers/armController.py
+++ radio/app/controllers/armController.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import time
+import logging
 from typing import TYPE_CHECKING
 
 from app.customTypes import Response
@@ -10,8 +11,11 @@ from pymavlink import mavutil
 if TYPE_CHECKING:
     from app.drone import Drone
 
+logger = logging.getLogger("fgcs")
 
 class ArmController:
+    
+    
     def __init__(self, drone: Drone) -> None:
         """
         The Arm controller controls all arm/disarming operations.
@@ -32,6 +36,7 @@ class ArmController:
             Response: The response from the arm command
         """
         if self.drone.armed:
+            logger.warning("Arm failed: Drone already armed")
             return {"success": False, "message": "Already armed"}
 
         self.drone.is_listening = False
@@ -51,16 +56,18 @@ class ArmController:
                 self.drone.logger.debug("Waiting for arm")
                 while not self.drone.armed:
                     time.sleep(0.05)
-                self.drone.logger.debug("ARMED")
+                logger.debug("Armed successfully")
                 return {"success": True, "message": "Armed successfully"}
             else:
-                self.drone.logger.debug("Arming failed")
+                logger.warning("Arm failed: command not accepted")
         except Exception as e:
             self.drone.is_listening = True
-            self.drone.logger.error(e, exc_info=True)
+            logger.error(e, exc_info=True)
             if self.drone.droneErrorCb:
                 self.drone.droneErrorCb(str(e))
             return {"success": False, "message": "Could not arm, serial exception"}
+        
+        logger.warning("Arm failed: unknown error")
         return {"success": False, "message": "Could not arm, command not accepted"}
 
     def disarm(self, force: bool = False) -> Response:
@@ -74,6 +81,7 @@ class ArmController:
             Response: The response from the disarm command
         """
         if not self.drone.armed:
+            logger.warning("Disarm failed: Drone is already disarmed")
             return {"success": False, "message": "Already disarmed"}
 
         self.drone.is_listening = False
@@ -93,15 +101,18 @@ class ArmController:
                 self.drone.logger.debug("Waiting for disarm")
                 while self.drone.armed:
                     time.sleep(0.05)
-                self.drone.logger.debug("DISARMED")
+                logger.debug("Disarmed successfully")
                 return {"success": True, "message": "Disarmed successfully"}
             else:
-                self.drone.logger.debug("Could not disarm, command not accepted")
+                logger.warning("Disarm failed: command not accepted")
         except Exception as e:
             self.drone.is_listening = True
-            self.drone.logger.error(e, exc_info=True)
+            logger.error(e, exc_info=True)
+            
             if self.drone.droneErrorCb:
                 self.drone.droneErrorCb(str(e))
+                
             return {"success": False, "message": "Could not disarm, serial exception"}
 
+        logger.warning("Disarm failed: unknown error")
         return {"success": False, "message": "Could not disarm, command not accepted"}

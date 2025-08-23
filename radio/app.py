0f1dd078010c19fe614a87077dfc1f2e72bf361c
Julian Jones <julianjones663@gmail.com>
2025-08-23 10:33:37 +0100

    reverted change

--- radio/app.py
+++ radio/app.py
@@ -6,8 +6,6 @@ from dotenv import load_dotenv
 
 app = create_app(debug=True)
 
-# DEBUG STATEMENT FOR TESTING #645
-
 if __name__ == "__main__":
     print("Loading dotenv.")
     env_path = Path("../gcs/.env")

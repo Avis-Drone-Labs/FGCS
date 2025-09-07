50c29a6abff0cbae988e34cb815b69c278361d68
Kush Makkapati <kush.makkapati@icloud.com>
2025-09-07 19:15:48 +0100

    Add link stats popout window (#675)
    * Add backend implementation for link stats
    
    * Add link stats window
    
    * Round bytes and packets per second values

---
+++ gcs/electron/modules/linkStatsWindow.ts
@@ -0,0 +1,64 @@
+import { BrowserWindow, ipcMain } from "electron"
+import path from "path"
+
+const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"]
+
+let linkStatsWin: BrowserWindow | null = null
+
+export function openLinkStatsWindow() {
+  if (linkStatsWin === null) {
+    linkStatsWin = new BrowserWindow({
+      width: 500,
+      height: 300,
+      frame: true,
+      icon: path.join(process.env.VITE_PUBLIC, "app_icon.ico"),
+      show: false,
+      title: "Connection Stats",
+      webPreferences: {
+        preload: path.join(__dirname, "preload.js"),
+        contextIsolation: true,
+      },
+      fullscreen: false,
+      fullscreenable: false,
+    })
+  }
+
+  if (VITE_DEV_SERVER_URL) {
+    linkStatsWin?.loadURL(VITE_DEV_SERVER_URL + "linkStats.html")
+  } else {
+    linkStatsWin?.loadFile(path.join(process.env.DIST, "linkStats.html"))
+  }
+
+  // Windows doesn't consider maximising to be fullscreening so we must prevent default
+  linkStatsWin.on("maximize", (e: Event) => {
+    e.preventDefault()
+  })
+  linkStatsWin.on("close", () => {
+    linkStatsWin = null
+  })
+  linkStatsWin.setMenuBarVisibility(false)
+  linkStatsWin.show()
+}
+
+export function closeLinkStatsWindow() {
+  destroyLinkStatsWindow()
+}
+
+export function destroyLinkStatsWindow() {
+  linkStatsWin?.close()
+  linkStatsWin = null
+}
+
+export default function registerLinkStatsIPC() {
+  ipcMain.handle("openLinkStatsWindow", () => {
+    openLinkStatsWindow()
+  })
+  ipcMain.handle("closeLinkStatsWindow", () => closeLinkStatsWindow())
+  ipcMain.handle("update-link-stats", (_, linkStats) => {
+    linkStatsWin?.webContents.send("send-link-stats", linkStats)
+    const uptimeFormatted = new Date(Math.round(linkStats.uptime) * 1000)
+      .toISOString()
+      .substring(11, 19)
+    linkStatsWin?.setTitle(`Connection Stats | Uptime: ${uptimeFormatted}`)
+  })
+}

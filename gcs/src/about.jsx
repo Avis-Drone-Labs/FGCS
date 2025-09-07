9fd1c53fb9a720b35d336ce5e016e7fe02132d10
Kush Makkapati <kush.makkapati@icloud.com>
2025-09-07 16:31:16 +0100

    Add custom about window (#673)
    * Add initial about window
    
    * Update styling for about window
    
    * Address copilot review comments

---
+++ gcs/src/about.jsx
@@ -0,0 +1,21 @@
+import "./css/index.css" // Needs to be at the top of the file
+import "./css/resizable.css"
+
+// Style imports
+import "@mantine/code-highlight/styles.css"
+import "@mantine/core/styles.css"
+import "@mantine/notifications/styles.css"
+import "@mantine/spotlight/styles.css"
+import "@mantine/tiptap/styles.css"
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+import AboutWindow from "./components/aboutWindow/aboutWindow"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AboutWindow />
+  </React.StrictMode>,
+)
+
+postMessage({ payload: "removeLoading" }, "*")

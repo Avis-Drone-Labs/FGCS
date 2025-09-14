4781e448233b95772be1c6934bd2752fe69ae960
Kush Makkapati <kush.makkapati@icloud.com>
2025-09-14 13:00:50 +0100

    Fix mypy error

--- radio/app/endpoints/mission.py
+++ radio/app/endpoints/mission.py
@@ -108,9 +108,9 @@ def getCurrentMissionAll() -> None:
     socketio.emit(
         "current_mission_all",
         {
-            "mission_items": result.get("data", {}).get("mission_items", []),
-            "fence_items": result.get("data", {}).get("fence_items", []),
-            "rally_items": result.get("data", {}).get("rally_items", []),
+            "mission_items": result.get("data", {}).get("mission_items") or [],
+            "fence_items": result.get("data", {}).get("fence_items") or [],
+            "rally_items": result.get("data", {}).get("rally_items") or [],
         },
     )
 

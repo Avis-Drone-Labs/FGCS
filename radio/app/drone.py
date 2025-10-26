b71d67a01cd789f3851c03acdd79b83204c860d9
Kush Makkapati <kush.makkapati@icloud.com>
2025-10-26 14:36:04 +0000

    Address copilot review comment

--- radio/app/drone.py
+++ radio/app/drone.py
@@ -478,16 +478,8 @@ class Drone:
                 self.logger.error(e, exc_info=True)
             except KeyboardInterrupt:
                 break
-            except serial.serialutil.SerialException as e:
-                self.logger.error("Autopilot disconnected")
-                self.logger.error(e, exc_info=True)
-                if self.droneDisconnectCb:
-                    self.droneDisconnectCb()
-                self.close()
-                break
-            except ConnectionAbortedError as e:
-                self.logger.error("Connection to autopilot aborted")
-                self.logger.error(e, exc_info=True)
+            except (serial.serialutil.SerialException, ConnectionAbortedError):
+                self.logger.error("Autopilot disconnected", exc_info=True)
                 if self.droneDisconnectCb:
                     self.droneDisconnectCb()
                 self.close()

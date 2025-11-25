8e95791f9633b5fc1121b2c572d731b9e1f2c14f
Matthew-Dobson <mjadobson1@sheffield.ac.uk>
2025-11-25 10:33:10 +0000

    Removed "precision" from HDoP tooltip

--- gcs/src/dashboard.jsx
+++ gcs/src/dashboard.jsx
@@ -204,7 +204,7 @@ export default function Dashboard() {
           <StatusSection
             icon={<IconTarget />}
             value={hdopDisplay}
-            tooltip="GPS HDOP (precision)"
+            tooltip="GPS HDoP"
           />
           <StatusSection
             icon={<IconGps />}

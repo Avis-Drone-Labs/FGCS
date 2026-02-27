634cb2ffe09aa3a861aa1b0a9323ebfa74922b49
Matthew-Dobson <mjadobson1@sheffield.ac.uk>
2026-02-27 19:31:01 +0000

    removed title from graph windows

--- gcs/src/graphWindow.jsx
+++ gcs/src/graphWindow.jsx
@@ -87,14 +87,7 @@ export default function GraphWindowApp() {
 
   return (
     <div className="w-full h-full bg-falcongrey-800 p-4 flex flex-col">
-      <div className="text-lg">{title}</div>
-
-      {desc ? (
-        <div className="text-sm text-falcongray-90 opacity-70 mt-1">{desc}</div>
-      ) : null}
-
-      {/* Min-h-0 prevents overflow sizing messing up */}
-      <div className="mt-4 flex-1 min-h-0">
+      <div className="flex-1 min-h-0">
         <RealtimeGraph
           ref={graphRef}
           datasetLabel={label}

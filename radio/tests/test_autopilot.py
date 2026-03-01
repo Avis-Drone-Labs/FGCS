9de72957638d10c0dfd5810cb979d013de0e869a
Ryan Turner <100864486+Turnlings@users.noreply.github.com>
2026-03-01 18:55:14 +0000

    Update event name in test

--- radio/tests/test_autopilot.py
+++ radio/tests/test_autopilot.py
@@ -14,7 +14,7 @@ def test_reboot_success(socketio_client: SocketIOTestClient):
     socketio_result = socketio_client.get_received()
 
     event_names = [e["name"] for e in socketio_result]
-    assert "connecting" in event_names
+    assert "reboot_connecting" in event_names
 
     assert socketio_result[-1]["name"] == "reboot_autopilot"
     assert socketio_result[-1]["args"][0]["success"]
